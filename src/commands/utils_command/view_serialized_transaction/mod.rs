97c4d7953f71fd10fe996534072849e09233e6fc
FroVolod <36816899+FroVolod@users.noreply.github.com>
2021-06-17 08:09:44 +0300

    Serialized transaction (#22)
    * serialized-transaction: added
    
    * serialized-transaction: added GUIDE
    
    * serialized-transaction: fixed

---
+++ src/commands/utils_command/view_serialized_transaction/mod.rs
@@ -0,0 +1,51 @@
+use dialoguer::Input;
+use near_primitives::borsh::BorshDeserialize;
+
+/// Using this utility, you can view the contents of a serialized transaction (signed or not).
+#[derive(Debug, Default, clap::Clap)]
+pub struct CliViewSerializedTransaction {
+    transaction: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct ViewSerializedTransaction {
+    transaction: String,
+}
+
+impl From<CliViewSerializedTransaction> for ViewSerializedTransaction {
+    fn from(item: CliViewSerializedTransaction) -> Self {
+        let transaction: String = match item.transaction {
+            Some(transaction) => transaction,
+            None => ViewSerializedTransaction::input_transaction(),
+        };
+        Self { transaction }
+    }
+}
+
+impl ViewSerializedTransaction {
+    fn input_transaction() -> String {
+        Input::new()
+            .with_prompt("Enter the hash of the transaction")
+            .interact_text()
+            .unwrap()
+    }
+
+    pub async fn process(self) -> crate::CliResult {
+        let serialize_from_base64 =
+            near_primitives::serialize::from_base64(&self.transaction).unwrap();
+        match near_primitives::transaction::Transaction::try_from_slice(&serialize_from_base64) {
+            Ok(transaction) => println!("\n{:#?}", &transaction),
+            Err(_) => {
+                match near_primitives::transaction::SignedTransaction::try_from_slice(
+                    &serialize_from_base64,
+                ) {
+                    Ok(signed_transaction) => println!("\n{:#?}", signed_transaction),
+                    Err(err) => {
+                        println!("\nError: Base64 transaction sequence is invalid: {}", err)
+                    }
+                }
+            }
+        };
+        Ok(())
+    }
+}
