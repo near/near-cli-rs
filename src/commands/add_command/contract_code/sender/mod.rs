8d6eae0fad82848912a563e5aa6380c76a58b676
FroVolod <frol_off@meta.ua>
2021-07-23 14:14:59 +0300

    check-account: add contract

--- src/commands/add_command/contract_code/sender/mod.rs
+++ src/commands/add_command/contract_code/sender/mod.rs
@@ -25,8 +25,20 @@ impl Sender {
         connection_config: Option<crate::common::ConnectionConfig>,
     ) -> color_eyre::eyre::Result<Self> {
         let sender_account_id: String = match item.sender_account_id {
-            Some(cli_sender_account_id) => cli_sender_account_id,
-            None => Sender::input_sender_account_id(),
+            Some(cli_sender_account_id) => match &connection_config {
+                Some(network_connection_config) => match crate::common::check_account_id(
+                    network_connection_config.clone(),
+                    cli_sender_account_id.clone(),
+                )? {
+                    Some(_) => cli_sender_account_id,
+                    None => {
+                        println!("This account ID <{}> doesn't exist", cli_sender_account_id);
+                        Sender::input_sender_account_id(connection_config.clone())?
+                    }
+                },
+                None => cli_sender_account_id,
+            },
+            None => Sender::input_sender_account_id(connection_config.clone())?,
         };
         let contract = match item.contract {
             Some(cli_contract) => super::contract::Contract::from(
@@ -47,12 +59,28 @@ impl Sender {
 }
 
 impl Sender {
-    pub fn input_sender_account_id() -> String {
-        println!();
-        Input::new()
-            .with_prompt("What is the account ID of the contract?")
-            .interact_text()
-            .unwrap()
+    fn input_sender_account_id(
+        connection_config: Option<crate::common::ConnectionConfig>,
+    ) -> color_eyre::eyre::Result<String> {
+        match &connection_config {
+            Some(connection_config) => loop {
+                let account_id: String = Input::new()
+                    .with_prompt("What is the account ID of the contract?")
+                    .interact_text()
+                    .unwrap();
+                match crate::common::check_account_id(
+                    connection_config.clone(),
+                    account_id.clone(),
+                )? {
+                    Some(_) => break Ok(account_id),
+                    None => println!("This account ID <{}> doesn't exist", account_id),
+                };
+            },
+            None => Ok(Input::new()
+                .with_prompt("What is the account ID of the contract?")
+                .interact_text()
+                .unwrap()),
+        }
     }
 
     pub async fn process(
