390eb4bd051fcac26cbd3d8c16e4ba26b5dd4de5
FroVolod <frol_off@meta.ua>
2021-07-25 18:28:28 +0300

    check-account: transfer->fixed "checking the balance of the amount"

--- src/commands/transfer_command/transfer_near_tokens_type/mod.rs
+++ src/commands/transfer_command/transfer_near_tokens_type/mod.rs
@@ -103,13 +103,18 @@ impl TransferNEARTokensAction {
                                 "You need to enter a value of no more than {}",
                                 crate::common::NearBalance::from_yoctonear(account_balance)
                             );
-                            TransferNEARTokensAction::input_amount(account_balance)
+                            TransferNEARTokensAction::input_amount(Some(account_balance))
                         }
                     }
-                    None => TransferNEARTokensAction::input_amount(account_balance),
+                    None => TransferNEARTokensAction::input_amount(Some(account_balance)),
                 }
             }
-            None => TransferNEARTokensAction::input_amount(0),
+            None => {
+                match item.amount {
+                    Some(cli_amount) => cli_amount,
+                    None => TransferNEARTokensAction::input_amount(None),
+                }
+            }
         };
         let sign_option = match item.sign_option {
             Some(cli_sign_transaction) => crate::commands::construct_transaction_command::sign_transaction::SignTransaction::from(cli_sign_transaction, connection_config, sender_account_id)?,
@@ -123,21 +128,27 @@ impl TransferNEARTokensAction {
 }
 
 impl TransferNEARTokensAction {
-    fn input_amount(account_balance: u128) -> crate::common::NearBalance {
-        loop {
-            let input_amount: crate::common::NearBalance = Input::new()
-                        .with_prompt("How many NEAR Tokens do you want to transfer? (example: 10NEAR or 0.5near or 10000yoctonear)")
-                        .with_initial_text(format!("{}", crate::common::NearBalance::from_yoctonear(account_balance)))
-                        .interact_text()
-                        .unwrap();
-            if input_amount <= crate::common::NearBalance::from_yoctonear(account_balance) {
-                break input_amount;
-            } else {
-                println!(
-                    "You need to enter a value of no more than {}",
-                    crate::common::NearBalance::from_yoctonear(account_balance)
-                )
+    fn input_amount(account_balance: Option<u128>) -> crate::common::NearBalance {
+        match account_balance {
+            Some(account_balance) => loop {
+                let input_amount: crate::common::NearBalance = Input::new()
+                            .with_prompt("How many NEAR Tokens do you want to transfer? (example: 10NEAR or 0.5near or 10000yoctonear)")
+                            .with_initial_text(format!("{}", crate::common::NearBalance::from_yoctonear(account_balance)))
+                            .interact_text()
+                            .unwrap();
+                if input_amount <= crate::common::NearBalance::from_yoctonear(account_balance) {
+                    break input_amount;
+                } else {
+                    println!(
+                        "You need to enter a value of no more than {}",
+                        crate::common::NearBalance::from_yoctonear(account_balance)
+                    )
+                }
             }
+            None => Input::new()
+                        .with_prompt("How many NEAR Tokens do you want to transfer? (example: 10NEAR or 0.5near or 10000yoctonear)")
+                        .interact_text()
+                        .unwrap()
         }
     }
 
