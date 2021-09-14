1d0cf59eb574acea53871ae5bb824f3157578119
Phuong Nguyen <phuong@near.org>
2021-09-14 13:48:17 -0700

    Added impl for BlockHeight

--- cli-visual/src/impls.rs
+++ cli-visual/src/impls.rs
@@ -1,4 +1,4 @@
-use near_primitives::{hash::CryptoHash};
+use near_primitives::{hash::CryptoHash, types::BlockHeight};
 use crate::prompt::{PromptMessage, Interactive};
 
 impl PromptMessage for CryptoHash {
@@ -8,6 +8,18 @@ impl PromptMessage for CryptoHash {
 }
 
 impl Interactive<Self> for CryptoHash {
+    fn interactive(self) -> Self {
+        self
+    }
+}
+
+impl PromptMessage for BlockHeight {
+    fn prompt_msg() -> String {
+        "Type the block ID height for this account".to_string()
+    }
+}
+
+impl Interactive<Self> for BlockHeight {
     fn interactive(self) -> Self {
         self
     }
