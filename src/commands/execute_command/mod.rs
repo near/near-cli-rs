c18bdecc75ec5ce9cde33dc66a090e8ce1990586
Serhii Volovyk <SergeyVolovyk@gmail.com>
2021-10-19 19:31:11 +0300

    chore: fmt fix (#74)

--- src/commands/execute_command/mod.rs
+++ src/commands/execute_command/mod.rs
@@ -68,9 +68,13 @@ enum CliMethod {
 #[derive(Debug, Clone, EnumDiscriminants)]
 #[strum_discriminants(derive(EnumMessage, EnumIter))]
 enum Method {
-    #[strum_discriminants(strum(message = "Execute a changing method (construct a transaction with a function call)"))]
+    #[strum_discriminants(strum(
+        message = "Execute a changing method (construct a transaction with a function call)"
+    ))]
     ChangeMethod(self::change_method::operation_mode::OperationMode),
-    #[strum_discriminants(strum(message = "Execute a viewing method (read-only call, which does not require a transaction)"))]
+    #[strum_discriminants(strum(
+        message = "Execute a viewing method (read-only call, which does not require a transaction)"
+    ))]
     ViewMethod(self::view_method::operation_mode::OperationMode),
 }
 
