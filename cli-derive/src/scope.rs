c0994ad3cde15e68fcc62aeb3618681ef24ebbb9
Phuong Nguyen <phuong@near.org>
2021-11-15 23:33:49 +0000

    Restructured scope.rs

--- cli-derive/src/scope.rs
+++ cli-derive/src/scope.rs
@@ -4,13 +4,31 @@ use quote::{quote};
 
 
 pub fn gen(args: &StructArgs) -> TokenStream {
+    let struct_ident = &args.ident;
+    let scope_ident = format!("{}Scope", struct_ident);
+    let scope_ident = syn::Ident::new(&scope_ident, struct_ident.span());
+    let fields = gen_scope_internals(args);
+
+    quote! {
+        #[derive(Clone)]
+        struct #scope_ident {
+            #(#fields)*
+        }
+
+        impl near_cli_visual::types::Scoped for #struct_ident {
+            type Scope = #scope_ident;
+        }
+    }
+}
+
+fn gen_scope_internals(args: &StructArgs) -> Vec<TokenStream> {
     let StructArgs {
-        ident,
+        ident: struct_ident,
         generics: _,
         data: _,
     } = args;
 
-    let fields = args.fields().into_iter().map(|f| {
+    args.fields().into_iter().map(|f| {
         let FieldArgs {
             ident,
             ty,
@@ -18,23 +36,17 @@ pub fn gen(args: &StructArgs) -> TokenStream {
             ..
         } = f;
 
-        // will fail if enum, newtype or tuple
-        let ident = ident.as_ref().expect("only supported for regular structs");
         if *subcommand {
             // Subcommand are not apart of the Scope. So exclude it with empty field.
-            quote! {}
-        } else {
-            quote! {
-                #ident: #ty,
-            }
+            return quote!();
         }
-    });
 
-    let name = format!("{}Scope", ident);
-    let name = syn::Ident::new(&name, ident.span());
-    quote! {
-        struct #name {
-            #(#fields)*
+        // will fail if enum, newtype or tuple
+        let ident = ident.as_ref().expect("only supported for regular structs");
+
+        quote! {
+            pub #ident: #ty,
         }
-    }
+    })
+    .collect()
 }
