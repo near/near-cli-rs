3e8b75356fde2ff44fb23a3dd49b5fcc4275e7b1
Phuong Nguyen <phuong@near.org>
2021-11-17 00:40:57 +0000

    Update example to be use more up to date codegen

--- cli-transfer/src/main.rs
+++ cli-transfer/src/main.rs
@@ -1,22 +1,50 @@
 use near_cli_derive::Eclap;
+use near_cli_visual::types::{BuilderFrom, Validate, Scoped};
 
-
-#[derive(Eclap)]
+#[derive(Debug, Eclap)]
+// #[eclap(enable(validator))]
 struct A {
     #[eclap(single, subcommand)]
     bar: B,
 
-    #[eclap(prompt_msg = "For what baz?")]
+    #[eclap(prompt_msg = "To baz or not to?")]
     baz: bool,
 }
 
-#[derive(Eclap)]
+impl Validate for A {
+    type Err = ();
+
+    fn validate(clap: Option<&Self::Clap>, builder: &Self::Builder) -> Result<(), Self::Err> {
+        Ok(())
+    }
+}
+
+#[derive(Debug, Eclap)]
 struct B {
-    #[eclap(prompt_msg = "For what bar?")]
+    #[eclap(prompt_msg = "To bar or not to?")]
     bar: bool,
+}
+
+impl BuilderFrom<A> for B {
+    fn builder_from(a: &<A as Scoped>::Scope) -> Self::Builder {
+        Self::Builder {
+            bar: None,
+        }
+    }
+}
+
+impl Validate for B {
+    type Err = ();
+
+    fn validate(clap: Option<&Self::Clap>, builder: &Self::Builder) -> Result<(), Self::Err> {
+        Ok(())
+    }
 }
 
 
 fn main() {
+    use near_cli_visual::types::InteractiveParse;
 
+    let a = A::iparse();
+    println!("{:?}", a);
 }
