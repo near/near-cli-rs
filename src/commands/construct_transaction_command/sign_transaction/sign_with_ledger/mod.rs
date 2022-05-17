24e57850a6634fba153f038e544c6eaf1dbb0600
Vlad Frolov <frolvlad@gmail.com>
2022-05-17 18:20:38 +0200

    feat: Migrate away from actix to pure tokio

--- src/commands/construct_transaction_command/sign_transaction/sign_with_ledger/mod.rs
+++ src/commands/construct_transaction_command/sign_transaction/sign_with_ledger/mod.rs
@@ -34,7 +34,8 @@ impl SignLedger {
             "Please allow getting the PublicKey on Ledger device (HD Path: {})",
             seed_phrase_hd_path
         );
-        let public_key = actix::System::new()
+        let public_key = tokio::runtime::Runtime::new()
+            .unwrap()
             .block_on(async {
                 near_ledger::get_public_key(seed_phrase_hd_path.clone().into()).await
             })
