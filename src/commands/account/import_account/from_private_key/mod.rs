c04e9aaceeac72e12e499308c8e2d8b65c262337
vova <frol_off@meta.ua>
2022-12-15 09:39:16 +0200

    modified LoginFromPrivateKey

--- src/commands/account/import_account/from_private_key/mod.rs
+++ src/commands/account/import_account/from_private_key/mod.rs
@@ -9,16 +9,22 @@ pub struct LoginFromPrivateKey {
     network_config: crate::network::Network,
 }
 
+#[derive(Debug, serde::Serialize)]
+struct KeyPairProperties {
+    public_key: near_crypto::PublicKey,
+    private_key: near_crypto::SecretKey,
+}
+
 impl LoginFromPrivateKey {
     pub async fn process(&self, config: crate::config::Config) -> crate::CliResult {
         let network_config = self.network_config.get_network_config(config.clone());
-        let secret_key: near_crypto::SecretKey = self.private_key.clone().into();
-        let public_key = secret_key.public_key();
-        let key_pair_properties_buf = serde_json::json!({
-            "public_key": public_key.to_string(),
-            "private_key": secret_key.to_string(),
-        })
-        .to_string();
+        let private_key: near_crypto::SecretKey = self.private_key.clone().into();
+        let public_key = private_key.public_key();
+        let key_pair_properties = KeyPairProperties {
+            public_key: public_key.clone(),
+            private_key
+        };
+        let key_pair_properties_buf = serde_json::to_string(&key_pair_properties).unwrap();
         let error_message = "\nIt is currently not possible to verify the account access key.\nYou may have entered an incorrect account_id.\nYou have the option to reconfirm your account or save your access key information.\n";
         super::login(
             network_config,
