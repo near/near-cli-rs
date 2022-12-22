231d3bd726574db0daf3fb226133826d99a330cd
FroVolod <frol_off@meta.ua>
2022-12-22 18:37:20 +0200

    added "keys"

--- src/js_command_match/mod.rs
+++ src/js_command_match/mod.rs
@@ -1,5 +1,6 @@
 mod create_account;
 mod delete;
+mod keys;
 mod state;
 
 #[derive(Debug, Clone, clap::Parser)]
@@ -7,6 +8,7 @@ pub enum JsCmd {
     CreateAccount(self::create_account::CreateAccountArgs),
     State(self::state::StateArgs),
     Delete(self::delete::DeleteArgs),
+    Keys(self::keys::KeysArgs),
 }
 
 impl JsCmd {
@@ -15,6 +17,7 @@ impl JsCmd {
             Self::CreateAccount(create_account_args) => create_account_args.to_cli_args(),
             Self::State(state_args) => state_args.to_cli_args(),
             Self::Delete(delete_args) => delete_args.to_cli_args(),
+            Self::Keys(keys_args) => keys_args.to_cli_args(),
         }
     }
 }
