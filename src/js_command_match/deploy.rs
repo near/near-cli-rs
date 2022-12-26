69185feac386282baf6628a8987d0a62e76b05cc
FroVolod <frol_off@meta.ua>
2022-12-26 18:17:03 +0200

    fixed "deploy"

--- src/js_command_match/deploy.rs
+++ src/js_command_match/deploy.rs
@@ -1,16 +1,14 @@
 #[derive(Debug, Clone, clap::Parser)]
 pub struct DeployArgs {
-    #[clap(long = "accountId")]
     contract_account_id: String,
-    #[clap(long = "wasmFile")]
     wasm_file: String,
-    #[clap(long = "initFunction")]
+    #[clap(long, aliases = ["init_function", "initFunction"])]
     init_function: Option<String>,
-    #[clap(long = "initArgs", default_value = "{}")]
+    #[clap(long, aliases = ["init_args", "initArgs"], default_value = "{}")]
     init_args: String,
-    #[clap(long = "initGas", default_value_t = 30_000_000_000_000)]
+    #[clap(long, aliases = ["init_gas", "initGas"], default_value_t = 30_000_000_000_000)]
     init_gas: u64,
-    #[clap(long = "initDeposit", default_value = "0")]
+    #[clap(long, aliases = ["init_deposit", "initDeposit"], default_value = "0")]
     init_deposit: String,
 }
 
