3b7e8b6c1f30270fa1a54963f054cba54ee1e629
FroVolod <frol_off@meta.ua>
2022-12-26 09:29:00 +0200

    fixed "dev-deploy"

--- src/js_command_match/dev_deploy.rs
+++ src/js_command_match/dev_deploy.rs
@@ -1,10 +1,17 @@
 #[derive(Debug, Clone, clap::Parser)]
 pub struct DevDeployArgs {
+    #[clap(long = "wasmFile")]
     wasm_file: String,
-    init_function: String,
-    init_args: String,
-    init_gas: String,
+    #[clap(long = "initFunction")]
+    init_function: Option<String>,
+    #[clap(long = "initArgs")]
+    init_args: Option<String>,
+    #[clap(long = "initGas", default_value_t = 30_000_000_000_000)]
+    init_gas: u64,
+    #[clap(long = "initDeposit", default_value = "0")]
     init_deposit: String,
+    #[clap(long = "initialBalance", default_value = "100")]
     initial_balance: String,
-    force: String,
+    #[clap(long = "force", default_value_t = false)]
+    force: bool,
 }
