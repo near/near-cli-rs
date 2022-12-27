46383d19f682fb4425b4b15a28a817572ad57761
FroVolod <frol_off@meta.ua>
2022-12-27 21:14:18 +0200

    fixed "view-state"

--- src/js_command_match/view_state.rs
+++ src/js_command_match/view_state.rs
@@ -1,5 +1,12 @@
 #[derive(Debug, Clone, clap::Parser)]
 pub struct ViewStateArgs {
     account_id: String,
+    #[clap(long, default_value = "")]
     prefix: String,
+    #[clap(long, aliases = ["block_id", "blockId"], default_value = "0")]
+    block_id: String,
+    #[clap(long, default_value = "final")]
+    finality: String,
+    #[clap(long, default_value = "false")]
+    utf8: String,
 }
