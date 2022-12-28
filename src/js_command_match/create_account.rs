d72933a7f482a01c1b9919377728d6b5b33f84fd
FroVolod <frol_off@meta.ua>
2022-12-28 11:42:59 +0200

    fixed "create-account"

--- src/js_command_match/create_account.rs
+++ src/js_command_match/create_account.rs
@@ -1,23 +1,50 @@
 #[derive(Debug, Clone, clap::Parser)]
 pub struct CreateAccountArgs {
     account_id: String,
+    #[clap(long, aliases = ["master_account", "masterAccount"])]
+    master_account: String,
+    #[clap(long, aliases = ["public_key", "publicKey"], default_value = None)]
+    public_key: Option<String>,
+    #[clap(long, aliases = ["new_ledger_key", "newLedgerKey"], default_value = "44'/397'/0'/0'/1'")]
+    new_ledger_key: String,
+    #[clap(long, aliases = ["initial_balance", "initialBalance"], default_value = "100")]
+    initial_balance: String,
 }
 
 impl CreateAccountArgs {
     pub fn to_cli_args(&self, network_config: String) -> Vec<String> {
-        vec![
-            "account".to_owned(),
-            "create-account".to_owned(),
-            "fund-myself".to_owned(),
-            self.account_id.to_owned(),
-            "100 NEAR".to_owned(),
-            "autogenerate-new-keypair".to_owned(),
-            "save-to-keychain".to_owned(),
-            "sign-as".to_owned(),
-            "network-config".to_owned(),
-            network_config,
-            "sign-with-keychain".to_owned(),
-            "send".to_owned(),
-        ]
+        if let Some(public_key) = self.public_key.as_deref() {
+            vec![
+                "account".to_owned(),
+                "create-account".to_owned(),
+                "fund-myself".to_owned(),
+                self.account_id.to_owned(),
+                format!("{} NEAR", self.initial_balance),
+                "use-manually-provided-public-key".to_owned(),
+                public_key.to_owned(),
+                "sign-as".to_owned(),
+                self.master_account.to_owned(),
+                "network-config".to_owned(),
+                network_config,
+                "sign-with-keychain".to_owned(),
+                "send".to_owned(),
+            ]
+        } else {
+            vec![
+                "account".to_owned(),
+                "create-account".to_owned(),
+                "fund-myself".to_owned(),
+                self.account_id.to_owned(),
+                format!("{} NEAR", self.initial_balance),
+                "autogenerate-new-keypair".to_owned(),
+                "save-to-keychain".to_owned(),
+                "sign-as".to_owned(),
+                self.master_account.to_owned(),
+                "network-config".to_owned(),
+                network_config,
+                "sign-with-keychain".to_owned(),
+                "send".to_owned(),
+            ]
+        }
     }
 }
