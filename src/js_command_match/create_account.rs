a8637a15c90cc9051c2378632f9b6a354fdc1b86
FroVolod <frol_off@meta.ua>
2022-12-31 13:37:52 +0200

    fixed "create-account"

--- src/js_command_match/create_account.rs
+++ src/js_command_match/create_account.rs
@@ -3,16 +3,33 @@ pub struct CreateAccountArgs {
     account_id: String,
     #[clap(long, aliases = ["master_account", "masterAccount"])]
     master_account: String,
-    #[clap(long, aliases = ["public_key", "publicKey"], default_value = None)]
+    #[clap(long, aliases = ["public_key", "publicKey"], default_value = None, conflicts_with = "new_ledger_key")]
     public_key: Option<String>,
-    #[clap(long, aliases = ["new_ledger_key", "newLedgerKey"], default_value = "44'/397'/0'/0'/1'")]
-    new_ledger_key: String,
+    #[clap(long, aliases = ["new_ledger_key", "newLedgerKey"], default_missing_value = Some("44'/397'/0'/0'/1'"), num_args=0..=1)]
+    new_ledger_key: Option<String>,
     #[clap(long, aliases = ["initial_balance", "initialBalance"], default_value = "100")]
     initial_balance: String,
 }
 
 impl CreateAccountArgs {
     pub fn to_cli_args(&self, network_config: String) -> Vec<String> {
+        if self.new_ledger_key.is_some() {
+            return vec![
+                "account".to_owned(),
+                "create-account".to_owned(),
+                "fund-myself".to_owned(),
+                self.account_id.to_owned(),
+                format!("{} NEAR", self.initial_balance),
+                "use-ledger".to_owned(),
+                "save-to-keychain".to_owned(),
+                "sign-as".to_owned(),
+                self.master_account.to_owned(),
+                "network-config".to_owned(),
+                network_config,
+                "sign-with-keychain".to_owned(),
+                "send".to_owned(),
+            ];
+        }
         if let Some(public_key) = self.public_key.as_deref() {
             vec![
                 "account".to_owned(),
