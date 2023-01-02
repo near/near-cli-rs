61649cac7678aa48fb5338a0ff2c92b52974486d
FroVolod <frol_off@meta.ua>
2023-01-02 21:07:23 +0200

    fixed "add-key"

--- src/js_command_match/add_key.rs
+++ src/js_command_match/add_key.rs
@@ -4,8 +4,8 @@ pub struct AddKeyArgs {
     access_key: String,
     #[clap(long, aliases = ["contract_id", "contractId"], default_value = None)]
     contract_id: Option<String>,
-    #[clap(long, aliases = ["method_names", "methodNames"], default_value = None, value_delimiter = ',', num_args = 1..)]
-    method_names: Option<Vec<String>>,
+    #[clap(long, aliases = ["method_names", "methodNames"], requires = "contract_id", value_delimiter = ',', num_args = 1..)]
+    method_names: Vec<String>,
     #[clap(long, default_value = "0")]
     allowance: String,
 }
@@ -13,26 +13,24 @@ pub struct AddKeyArgs {
 impl AddKeyArgs {
     pub fn to_cli_args(&self, network_config: String) -> Vec<String> {
         if let Some(contract_id) = self.contract_id.as_deref() {
-            if let Some(method_names) = self.method_names.as_deref() {
-                return vec![
-                    "account".to_owned(),
-                    "add-key".to_owned(),
-                    self.account_id.to_owned(),
-                    "grant-function-call-access".to_owned(),
-                    "--allowance".to_owned(),
-                    format!("{} NEAR", self.allowance),
-                    "--receiver-account-id".to_owned(),
-                    contract_id.to_owned(),
-                    "--method-names".to_owned(),
-                    method_names.join(","),
-                    "use-manually-provided-public-key".to_owned(),
-                    self.access_key.to_owned(),
-                    "network-config".to_owned(),
-                    network_config,
-                    "sign-with-keychain".to_owned(),
-                    "send".to_owned(),
-                ];
-            }
+            return vec![
+                "account".to_owned(),
+                "add-key".to_owned(),
+                self.account_id.to_owned(),
+                "grant-function-call-access".to_owned(),
+                "--allowance".to_owned(),
+                format!("{} NEAR", self.allowance),
+                "--receiver-account-id".to_owned(),
+                contract_id.to_owned(),
+                "--method-names".to_owned(),
+                self.method_names.join(","),
+                "use-manually-provided-public-key".to_owned(),
+                self.access_key.to_owned(),
+                "network-config".to_owned(),
+                network_config,
+                "sign-with-keychain".to_owned(),
+                "send".to_owned(),
+            ];
         }
         vec![
             "account".to_owned(),
