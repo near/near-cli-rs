47a9d5ae6a0805d0de16435888fc5271aed4c290
FroVolod <frol_off@meta.ua>
2023-02-16 17:17:17 +0200

    fixed SignWithSeedPhrase

--- src/transaction_signature_options/mod.rs
+++ src/transaction_signature_options/mod.rs
@@ -1,7 +1,7 @@
-use inquire::{CustomType, Select};
+use inquire::{CustomType};
 use near_primitives::borsh::BorshSerialize;
 use serde::Deserialize;
-use strum::{EnumDiscriminants, EnumIter, EnumMessage, IntoEnumIterator};
+use strum::{EnumDiscriminants, EnumIter, EnumMessage};
 
 pub mod sign_with_access_key_file;
 pub mod sign_with_keychain;
@@ -10,7 +10,7 @@ pub mod sign_with_ledger;
 #[cfg(target_os = "macos")]
 pub mod sign_with_macos_keychain;
 pub mod sign_with_private_key;
-// pub mod sign_with_seed_phrase;
+pub mod sign_with_seed_phrase;
 
 #[derive(Debug, EnumDiscriminants, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(context = crate::commands::TransactionContext)]
@@ -44,11 +44,11 @@ pub enum SignWith {
     ))]
     /// Sign the transaction using the account access key file (access-key-file.json)
     SignWithAccessKeyFile(self::sign_with_access_key_file::SignAccessKeyFile),
-    // #[strum_discriminants(strum(
-    //     message = "sign-with-seed-phrase            - Sign the transaction using the seed phrase"
-    // ))]
-    // /// Sign the transaction using the seed phrase
-    // SignWithSeedPhrase(self::sign_with_seed_phrase::SignSeedPhrase),
+    #[strum_discriminants(strum(
+        message = "sign-with-seed-phrase            - Sign the transaction using the seed phrase"
+    ))]
+    /// Sign the transaction using the seed phrase
+    SignWithSeedPhrase(self::sign_with_seed_phrase::SignSeedPhrase),
 }
 
 // from_cli ...
@@ -66,25 +66,18 @@ pub fn input_signer_private_key() -> color_eyre::eyre::Result<crate::types::secr
 
 pub async fn sign_with(
     network_config: crate::network_for_transaction::NetworkForTransactionArgs,
-    prepopulated_unsigned_transaction: near_primitives::transaction::Transaction,
-    config: crate::config::Config,
+    _prepopulated_unsigned_transaction: near_primitives::transaction::Transaction,
+    _config: crate::config::Config,
 ) -> color_eyre::eyre::Result<Option<near_primitives::views::FinalExecutionOutcomeView>> {
     match network_config.get_sign_option() {
         #[cfg(target_os = "macos")]
-        SignWith::SignWithMacosKeychain(sign_macos_keychain) => Ok(None),
+        SignWith::SignWithMacosKeychain(_) => Ok(None),
         SignWith::SignWithKeychain(_) => Ok(None),
         #[cfg(feature = "ledger")]
         SignWith::SignWithLedger(_) => Ok(None),
         SignWith::SignWithPlaintextPrivateKey(_) => Ok(None),
-        SignWith::SignWithAccessKeyFile(sign_access_key_file) => Ok(None),
-        // SignWith::SignWithSeedPhrase(sign_seed_phrase) => {
-        //     sign_seed_phrase
-        //         .process(
-        //             prepopulated_unsigned_transaction,
-        //             network_config.get_network_config(config),
-        //         )
-        //         .await
-        // }
+        SignWith::SignWithAccessKeyFile(_) => Ok(None),
+        SignWith::SignWithSeedPhrase(_) => Ok(None),
     }
 }
 //-----------------------------------------------------------------------------------
@@ -129,10 +122,6 @@ pub enum Submit {
     Display,
 }
 
-// impl interactive_clap::ToCli for Submit {
-//     type CliVariant = Submit;
-// }
-
 impl interactive_clap::FromCli for Submit {
     type FromCliContext = SubmitContext;
     type FromCliError = color_eyre::eyre::Error;
@@ -165,10 +154,7 @@ impl interactive_clap::FromCli for Submit {
                         },
                     };
                 };
-
                 crate::common::print_transaction_status(transaction_info, context.network_config)?;
-                // Ok(Some(transaction_info))
-
                 Ok(Some(Self::Send))
             }
             Some(CliSubmit::Display) => {
@@ -180,7 +166,6 @@ impl interactive_clap::FromCli for Submit {
                 );
                 println!("\nSerialize_to_base64:\n{}", &base64_transaction);
                 Ok(Some(Self::Display))
-                // Ok(None)
             }
             None => Self::choose_variant(context.clone()),
         }
@@ -196,53 +181,6 @@ impl interactive_clap::FromCli for Submit {
 //     }
 // }
 
-impl Submit {
-    // pub fn choose_submit() -> Self {
-    //     let variants = SubmitDiscriminants::iter().collect::<Vec<_>>();
-    //     let select_submit = Select::new("How would you like to proceed", variants)
-    //         .prompt()
-    //         .unwrap_or(SubmitDiscriminants::Display);
-    //     match select_submit {
-    //         SubmitDiscriminants::Send => Submit::Send,
-    //         SubmitDiscriminants::Display => Submit::Display,
-    //     }
-    // }
-
-    pub async fn process(
-        &self,
-        network_config: crate::config::NetworkConfig,
-        signed_transaction: near_primitives::transaction::SignedTransaction,
-        serialize_to_base64: String,
-    ) -> color_eyre::eyre::Result<Option<near_primitives::views::FinalExecutionOutcomeView>> {
-        match self {
-            Submit::Send => {
-                println!("Transaction sent ...");
-                let transaction_info = loop {
-                    let transaction_info_result = network_config.json_rpc_client()
-                        .call(near_jsonrpc_client::methods::broadcast_tx_commit::RpcBroadcastTxCommitRequest{signed_transaction: signed_transaction.clone()})
-                        .await;
-                    match transaction_info_result {
-                        Ok(response) => {
-                            break response;
-                        }
-                        Err(err) => match crate::common::rpc_transaction_error(err) {
-                            Ok(_) => {
-                                tokio::time::sleep(std::time::Duration::from_millis(100)).await
-                            }
-                            Err(report) => return color_eyre::eyre::Result::Err(report),
-                        },
-                    };
-                };
-                Ok(Some(transaction_info))
-            }
-            Submit::Display => {
-                println!("\nSerialize_to_base64:\n{}", &serialize_to_base64);
-                Ok(None)
-            }
-        }
-    }
-}
-
 #[derive(Debug, Deserialize)]
 pub struct AccountKeyPair {
     pub public_key: near_crypto::PublicKey,
@@ -253,5 +191,4 @@ pub struct AccountKeyPair {
 pub struct SubmitContext {
     pub network_config: crate::config::NetworkConfig,
     pub signed_transaction: near_primitives::transaction::SignedTransaction,
-    // pub base64_transaction: String,
 }
--- src/transaction_signature_options/sign_with_seed_phrase/mod.rs
+++ src/transaction_signature_options/sign_with_seed_phrase/mod.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 #[derive(Debug, Clone, interactive_clap_derive::InteractiveClap)]
 #[interactive_clap(context = crate::commands::TransactionContext)]
-// #[interactive_clap(skip_default_from_cli)]
+#[interactive_clap(skip_default_from_cli)]
 pub struct SignSeedPhrase {
     /// Enter the seed-phrase for this account
     master_seed_phrase: String,
@@ -13,47 +13,149 @@ pub struct SignSeedPhrase {
     submit: super::Submit,
 }
 
-// impl interactive_clap::FromCli for SignSeedPhrase {
-//     type FromCliContext = crate::commands::TransactionContext;
-//     type FromCliError = color_eyre::eyre::Error;
+#[derive(Clone)]
+pub struct SignSeedPhraseContext {
+    network_config: crate::config::NetworkConfig,
+    signed_transaction: near_primitives::transaction::SignedTransaction,
+}
 
-//     fn from_cli(
-//         optional_clap_variant: Option<<SignSeedPhrase as interactive_clap::ToCli>::CliVariant>,
-//         context: Self::FromCliContext,
-//     ) -> Result<Option<Self>, Self::FromCliError>
-//     where
-//         Self: Sized + interactive_clap::ToCli,
-//     {
-//         let master_seed_phrase: String = match optional_clap_variant
-//             .as_ref()
-//             .and_then(|clap_variant| clap_variant.master_seed_phrase.as_ref())
-//         {
-//             Some(master_seed_phrase) => master_seed_phrase.clone(),
-//             None => Self::input_master_seed_phrase(&context)?,
-//         };
-//         let seed_phrase_hd_path: crate::types::slip10::BIP32Path = match optional_clap_variant
-//             .as_ref()
-//             .and_then(|clap_variant| clap_variant.seed_phrase_hd_path.as_ref())
-//         {
-//             Some(seed_phrase_hd_path) => seed_phrase_hd_path.clone(),
-//             None => Self::input_seed_phrase_hd_path(&context)?,
-//         };
-//         let optional_submit = super::Submit::from_cli(
-//             optional_clap_variant.and_then(|clap_variant| clap_variant.submit),
-//             context,
-//         )?;
-//         let submit = if let Some(submit) = optional_submit {
-//             submit
-//         } else {
-//             return Ok(None);
-//         };
-//         Ok(Some(Self {
-//             master_seed_phrase,
-//             seed_phrase_hd_path,
-//             submit,
-//         }))
-//     }
-// }
+impl SignSeedPhraseContext {
+    pub fn from_previous_context(
+        previous_context: crate::commands::TransactionContext,
+        scope: &<SignSeedPhrase as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> Result<Self, color_eyre::eyre::Error> {
+        let network_config = previous_context.network_config.clone();
+
+        let key_pair_properties = crate::common::get_key_pair_properties_from_seed_phrase(
+            scope.seed_phrase_hd_path.clone(),
+            scope.master_seed_phrase.clone(),
+        )?;
+
+        let signer_secret_key: near_crypto::SecretKey =
+            near_crypto::SecretKey::from_str(&key_pair_properties.secret_keypair_str)?;
+        let signer_public_key =
+            near_crypto::PublicKey::from_str(&key_pair_properties.public_key_str)?;
+        let online_signer_access_key_response = tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(network_config.json_rpc_client().call(
+                near_jsonrpc_client::methods::query::RpcQueryRequest {
+                    block_reference: near_primitives::types::Finality::Final.into(),
+                    request: near_primitives::views::QueryRequest::ViewAccessKey {
+                        account_id: previous_context.transaction.signer_id.clone(),
+                        public_key: signer_public_key.clone(),
+                    },
+                },
+            ))
+            .map_err(|err| {
+                println!("\nYour transaction was not successfully signed.\n");
+                color_eyre::Report::msg(format!(
+                    "Failed to fetch public key information for nonce: {:?}",
+                    err
+                ))
+            })?;
+        let current_nonce =
+            if let near_jsonrpc_primitives::types::query::QueryResponseKind::AccessKey(
+                online_signer_access_key,
+            ) = online_signer_access_key_response.kind
+            {
+                online_signer_access_key.nonce
+            } else {
+                return Err(color_eyre::Report::msg("Error current_nonce".to_string()));
+            };
+
+        let mut unsigned_transaction = near_primitives::transaction::Transaction {
+            public_key: signer_public_key.clone(),
+            block_hash: online_signer_access_key_response.block_hash,
+            nonce: current_nonce + 1,
+            ..previous_context.transaction.clone()
+        };
+
+        (previous_context.on_before_signing_callback)(&mut unsigned_transaction, &network_config)?;
+
+        let signature = signer_secret_key.sign(unsigned_transaction.get_hash_and_size().0.as_ref());
+        let signed_transaction = near_primitives::transaction::SignedTransaction::new(
+            signature.clone(),
+            unsigned_transaction,
+        );
+
+        (previous_context.on_after_signing_callback)(&signed_transaction)?;
+
+        for action in signed_transaction.transaction.actions.iter() {
+            if let near_primitives::transaction::Action::FunctionCall(_) = action {
+                println!("\nSigned transaction:\n");
+                crate::common::print_transaction(signed_transaction.transaction.clone());
+            }
+        }
+
+        println!("\nYour transaction was signed successfully.");
+        println!("Public key: {}", signer_public_key);
+        println!("Signature: {}", signature);
+
+        Ok(Self {
+            network_config: previous_context.network_config,
+            signed_transaction,
+        })
+    }
+}
+
+impl From<SignSeedPhraseContext> for super::SubmitContext {
+    fn from(item: SignSeedPhraseContext) -> Self {
+        Self {
+            network_config: item.network_config,
+            signed_transaction: item.signed_transaction,
+        }
+    }
+}
+
+impl interactive_clap::FromCli for SignSeedPhrase {
+    type FromCliContext = crate::commands::TransactionContext;
+    type FromCliError = color_eyre::eyre::Error;
+
+    fn from_cli(
+        optional_clap_variant: Option<<SignSeedPhrase as interactive_clap::ToCli>::CliVariant>,
+        context: Self::FromCliContext,
+    ) -> Result<Option<Self>, Self::FromCliError>
+    where
+        Self: Sized + interactive_clap::ToCli,
+    {
+        let master_seed_phrase: String = match optional_clap_variant
+            .as_ref()
+            .and_then(|clap_variant| clap_variant.master_seed_phrase.as_ref())
+        {
+            Some(master_seed_phrase) => master_seed_phrase.clone(),
+            None => Self::input_master_seed_phrase(&context)?,
+        };
+        let seed_phrase_hd_path: crate::types::slip10::BIP32Path = match optional_clap_variant
+            .as_ref()
+            .and_then(|clap_variant| clap_variant.seed_phrase_hd_path.as_ref())
+        {
+            Some(seed_phrase_hd_path) => seed_phrase_hd_path.clone(),
+            None => Self::input_seed_phrase_hd_path(&context)?,
+        };
+        let new_context_scope = InteractiveClapContextScopeForSignSeedPhrase {
+            seed_phrase_hd_path: seed_phrase_hd_path.clone(),
+            master_seed_phrase: master_seed_phrase.clone(),
+        };
+        let seed_phrase_context =
+            SignSeedPhraseContext::from_previous_context(context.clone(), &new_context_scope)?;
+        let new_context = super::SubmitContext::from(seed_phrase_context.clone());
+
+        let optional_submit = super::Submit::from_cli(
+            optional_clap_variant.and_then(|clap_variant| clap_variant.submit),
+            new_context,
+        )?;
+        let submit = if let Some(submit) = optional_submit {
+            submit
+        } else {
+            return Ok(None);
+        };
+        Ok(Some(Self {
+            master_seed_phrase,
+            seed_phrase_hd_path,
+            submit,
+        }))
+    }
+}
 
 impl SignSeedPhrase {
     fn input_seed_phrase_hd_path(
@@ -64,30 +166,5 @@ impl SignSeedPhrase {
                 .with_default(crate::types::slip10::BIP32Path::from_str("m/44'/397'/0'").unwrap())
                 .prompt()?,
         )
-    }
-
-    pub async fn process(
-        &self,
-        prepopulated_unsigned_transaction: near_primitives::transaction::Transaction,
-        network_config: crate::config::NetworkConfig,
-    ) -> color_eyre::eyre::Result<Option<near_primitives::views::FinalExecutionOutcomeView>> {
-        let key_pair_properties = crate::common::get_key_pair_properties_from_seed_phrase(
-            self.seed_phrase_hd_path.clone(),
-            self.master_seed_phrase.clone(),
-        )?;
-        let sign_with_private_key = super::sign_with_private_key::SignPrivateKey {
-            signer_public_key: crate::types::public_key::PublicKey::from_str(
-                &key_pair_properties.public_key_str,
-            )?,
-            signer_private_key: crate::types::secret_key::SecretKey::from_str(
-                &key_pair_properties.secret_keypair_str,
-            )?,
-            nonce: None,
-            block_hash: None,
-            submit: self.submit.clone(),
-        };
-        sign_with_private_key
-            .process(prepopulated_unsigned_transaction, network_config)
-            .await
     }
 }
