0071565139207bac9c824af36a9b7669c366a2cf
Ivan Frolov <frolvanya@gmail.com>
2023-03-03 21:03:54 -0500

    feat: changed install location from ~/.local/bin to std::env::current_exe()

--- src/commands/extensions/self_update/mod.rs
+++ src/commands/extensions/self_update/mod.rs
@@ -1,8 +1,6 @@
-use std::io::Write;
-
 struct DownloadDirs {
     tmp_dir: tempfile::TempDir,
-    bin_dir: std::path::PathBuf,
+    install_path: std::path::PathBuf,
     archive_path: std::path::PathBuf,
     folder_path: std::path::PathBuf,
 }
@@ -15,7 +13,7 @@ impl SelfUpdateCommand {
     pub async fn process(&self) -> crate::CliResult {
         let self_clone = self.clone();
 
-        tokio::task::spawn_blocking(move || {
+        tokio::task::spawn_blocking(move || -> crate::CliResult {
             println!(
                 "Welcome to NEAR!\n
 This will download and install the official release of near-cli-rs."
@@ -59,52 +57,46 @@ This will download and install the official release of near-cli-rs."
 
             if current_version >= latest_release_version {
                 println!("\ninfo: near-cli-rs is already up to date\n");
-                Ok(())
             } else {
                 self_clone.download_release(&releases[0])?;
-                self_clone.export_path("$HOME/.local/bin")?;
 
                 println!("\nnear-cli-rs is installed now. Great!\n");
-
-                Ok(())
             }
+
+            Ok(())
         })
         .await?
     }
 
-    fn download_release(
-        &self,
-        release: &self_update::update::Release,
-    ) -> color_eyre::eyre::Result<self_update::update::ReleaseAsset> {
-        let home_dir = dirs::home_dir().expect("Failed to get home directory path");
-        let bin_dir = home_dir.join(".local/bin");
+    fn download_release(&self, release: &self_update::update::Release) -> crate::CliResult {
+        let install_path = match std::env::current_exe() {
+            Ok(path) => path,
+            Err(err) => {
+                return Err(color_eyre::Report::msg(format!(
+                    "Failed to get current near-cli location: {:?}",
+                    err
+                )))
+            }
+        };
 
-        if !bin_dir.is_dir() {
-            std::fs::create_dir(&bin_dir).map_err(|err| {
-                color_eyre::Report::msg(format!("Failed to create directory: {:?}", err))
-            })?;
-
-            println!("info: creating $HOME/.local/bin");
-        }
-
-        println!("near-cli-rs binary will be installed into the local bin home directory, located at:\n\n{}\n", bin_dir.display());
+        println!(
+            "near-cli-rs binary will be installed into the current near-cli-rs location:\n\n{}\n",
+            install_path.display()
+        );
 
         let triplet = self_update::get_target();
         println!("info: default host triple is {}", triplet);
 
-        let option_asset = release.asset_for(triplet);
         let unwrapped_asset;
 
-        if let Some(asset) = option_asset {
+        if let Some(asset) = release.asset_for(triplet, None) {
             unwrapped_asset = asset;
             println!("info: found release for {}", triplet);
         } else {
-            println!(
-                "error: could not find release for {}\n\nExiting...",
-                triplet
-            );
-
-            return Err(color_eyre::Report::msg("hello"));
+            return Err(color_eyre::Report::msg(format!(
+                "Failed to find release for {}",
+                triplet,
+            )));
         }
 
         let tmp_dir = tempfile::Builder::new()
@@ -139,21 +131,21 @@ This will download and install the official release of near-cli-rs."
 
         self.unpack_archive(
             &unwrapped_asset,
-            DownloadDirs {
+            &DownloadDirs {
                 tmp_dir,
-                bin_dir,
+                install_path,
                 archive_path,
                 folder_path,
             },
         )?;
 
-        Ok(unwrapped_asset)
+        Ok(())
     }
 
     fn unpack_archive(
         &self,
         asset: &self_update::update::ReleaseAsset,
-        download_dirs: DownloadDirs,
+        download_dirs: &DownloadDirs,
     ) -> crate::CliResult {
         println!("\ninfo: unpacking {} archive", asset.name);
         let tar_gz = flate2::read::GzDecoder::new(
@@ -166,82 +158,21 @@ This will download and install the official release of near-cli-rs."
             color_eyre::Report::msg(format!("Failed to unpack archive: {:?}", err))
         })?;
 
-        println!("info: moving near-cli binary to $HOME/.local/bin");
+        println!(
+            "info: moving near-cli binary to {}",
+            download_dirs.install_path.display()
+        );
         std::fs::copy(
             download_dirs.folder_path.join("near-cli"),
-            download_dirs.bin_dir.join("near-cli"),
+            &download_dirs.install_path,
         )
         .map_err(|err| {
             color_eyre::Report::msg(format!(
-                "Failed to copy near-cli binary to $HOME/.local/bin: {:?}",
+                "Failed to copy near-cli binary to {}: {:?}",
+                download_dirs.install_path.display(),
                 err
             ))
         })?;
-
-        Ok(())
-    }
-
-    fn export_path(&self, path: &str) -> crate::CliResult {
-        let mut export = true;
-
-        for path in env!("PATH").split(':') {
-            if std::path::Path::new(path).eq(&dirs::home_dir()
-                .expect("Failed to get path to home directory")
-                .join(".local/bin"))
-            {
-                export = false;
-                break;
-            }
-        }
-
-        if export {
-            println!(
-                "To get started you may need to restart your current shell.
-This would reload your PATH environment variable to include
-local's bin directory ($HOME/.local/bin)."
-            );
-
-            let home_dir = dirs::home_dir().unwrap();
-            let shell = env!("SHELL").split('/').last().unwrap_or("fruit");
-
-            let profile_file = match shell {
-                "bash" => ".bashrc",
-                "zsh" => ".zshrc",
-                "fish" => ".config/fish/config.fish",
-                _ => ".profile",
-            };
-            let profile_path = home_dir.join(profile_file);
-
-            let mut file = std::fs::OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open(&profile_path)
-                .map_err(|err| {
-                    color_eyre::Report::msg(format!(
-                        "Failed to open file `{}`: {:?}",
-                        profile_path.display(),
-                        err
-                    ))
-                })?;
-
-            let line = match shell {
-                "bash" => format!("export PATH={}:{}\n", "$PATH", path),
-                "zsh" => format!("export PATH={}:{}\n", path, "$PATH"),
-                "fish" => format!("set -gx PATH {} $PATH\n", path),
-                _ => format!("export PATH={}:{}\n", path, "$PATH"),
-            };
-
-            file.write(line.as_bytes()).map_err(|err| {
-                color_eyre::Report::msg(format!(
-                    "Failed to write to file `{}`: {:?}",
-                    profile_path.display(),
-                    err
-                ))
-            })?;
-            println!("info: PATH was added to {}", profile_path.display());
-        } else {
-            println!("info: $HOME/.local/bin is already in PATH variable");
-        }
 
         Ok(())
     }
