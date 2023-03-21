708d20e8843e68471160c932bd220f17e2e18870
FroVolod <frol_off@meta.ua>
2023-03-21 16:41:17 +0200

    refactored view_ft_balance

--- src/commands/tokens/mod.rs
+++ src/commands/tokens/mod.rs
@@ -3,7 +3,7 @@ use strum::{EnumDiscriminants, EnumIter, EnumMessage};
 mod send_ft;
 mod send_near;
 mod send_nft;
-// mod view_ft_balance;
+mod view_ft_balance;
 // mod view_near_balance;
 // mod view_nft_assets;
 
@@ -20,7 +20,7 @@ pub struct TokensCommands {
 #[derive(Debug, Clone)]
 pub struct TokensCommandsContext {
     config: crate::config::Config,
-    owner_account_id: crate::types::account_id::AccountId,
+    owner_account_id: near_primitives::types::AccountId,
 }
 
 impl TokensCommandsContext {
@@ -30,7 +30,7 @@ impl TokensCommandsContext {
     ) -> color_eyre::eyre::Result<Self> {
         Ok(Self {
             config: previous_context.0,
-            owner_account_id: scope.owner_account_id.clone(),
+            owner_account_id: scope.owner_account_id.clone().into(),
         })
     }
 }
@@ -66,9 +66,9 @@ pub enum TokensActions {
     // #[strum_discriminants(strum(message = "view-near-balance - View the balance of Near tokens"))]
     // ///View the balance of Near tokens
     // ViewNearBalance(self::view_near_balance::ViewNearBalance),
-    // #[strum_discriminants(strum(message = "view-ft-balance   - View the balance of FT tokens"))]
-    // ///View the balance of FT tokens
-    // ViewFtBalance(self::view_ft_balance::ViewFtBalance),
+    #[strum_discriminants(strum(message = "view-ft-balance   - View the balance of FT tokens"))]
+    ///View the balance of FT tokens
+    ViewFtBalance(self::view_ft_balance::ViewFtBalance),
     // #[strum_discriminants(strum(message = "view-nft-assets   - View the balance of NFT tokens"))]
     // ///View the balance of NFT tokens
     // ViewNftAssets(self::view_nft_assets::ViewNftAssets),
@@ -87,9 +87,7 @@ impl TokensActions {
             // }
             Self::SendFt(_) => Ok(()),
             Self::SendNft(_) => Ok(()),
-            // Self::ViewFtBalance(view_ft_balance) => {
-            //     view_ft_balance.process(config, owner_account_id).await
-            // }
+            Self::ViewFtBalance(_) => Ok(()),
             // Self::ViewNftAssets(view_nft_assets) => {
             //     view_nft_assets.process(config, owner_account_id).await
             // }
@@ -98,41 +96,40 @@ impl TokensActions {
 }
 
 #[derive(serde::Deserialize)]
-struct FtMetadata {
+pub struct FtMetadata {
     symbol: String,
     decimals: u64,
 }
 
-// async fn params_ft_metadata(
-//     config: crate::config::Config,
-//     ft_contract_account_id: crate::types::account_id::AccountId,
-//     network_config: crate::network_view_at_block::NetworkViewAtBlockArgs,
-// ) -> color_eyre::eyre::Result<FtMetadata> {
-//     let query_view_ft_metadata_response = network_config
-//         .get_network_config(config.clone())
-//         .json_rpc_client()
-//         .call(near_jsonrpc_client::methods::query::RpcQueryRequest {
-//             block_reference: network_config.get_block_ref(),
-//             request: near_primitives::views::QueryRequest::CallFunction {
-//                 account_id: ft_contract_account_id.clone().into(),
-//                 method_name: "ft_metadata".to_string(),
-//                 args: near_primitives::types::FunctionArgs::from(vec![]),
-//             },
-//         })
-//         .await
-//         .map_err(|err| {
-//             color_eyre::Report::msg(format!("Failed to fetch query for view method: {:?}", err))
-//         })?;
-//     let call_result =
-//         if let near_jsonrpc_primitives::types::query::QueryResponseKind::CallResult(result) =
-//             query_view_ft_metadata_response.kind
-//         {
-//             result.result
-//         } else {
-//             return Err(color_eyre::Report::msg("Error call result".to_string()));
-//         };
-//     let ft_metadata: FtMetadata = serde_json::from_slice(&call_result).map_err(|err| {
-//         color_eyre::Report::msg(format!("Impossible to get FT metadata! Error: {}", err))
-//     })?;
-//     Ok(ft_metadata)
-// }
+pub async fn params_ft_metadata(
+    ft_contract_account_id: near_primitives::types::AccountId,
+    network_config: &crate::config::NetworkConfig,
+    block_reference: near_primitives::types::BlockReference,
+) -> color_eyre::eyre::Result<FtMetadata> {
+    let query_view_ft_metadata_response = network_config
+        .json_rpc_client()
+        .call(near_jsonrpc_client::methods::query::RpcQueryRequest {
+            block_reference,
+            request: near_primitives::views::QueryRequest::CallFunction {
+                account_id: ft_contract_account_id.clone().into(),
+                method_name: "ft_metadata".to_string(),
+                args: near_primitives::types::FunctionArgs::from(vec![]),
+            },
+        })
+        .await
+        .map_err(|err| {
+            color_eyre::Report::msg(format!("Failed to fetch query for view method: {:?}", err))
+        })?;
+    let call_result =
+        if let near_jsonrpc_primitives::types::query::QueryResponseKind::CallResult(result) =
+            query_view_ft_metadata_response.kind
+        {
+            result.result
+        } else {
+            return Err(color_eyre::Report::msg("Error call result".to_string()));
+        };
+    let ft_metadata: FtMetadata = serde_json::from_slice(&call_result).map_err(|err| {
+        color_eyre::Report::msg(format!("Impossible to get FT metadata! Error: {}", err))
+    })?;
+    Ok(ft_metadata)
+}
--- src/commands/tokens/view_ft_balance/mod.rs
+++ src/commands/tokens/view_ft_balance/mod.rs
@@ -1,86 +1,108 @@
 use serde_json::json;
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
-#[interactive_clap(context = crate::GlobalContext)]
+#[interactive_clap(input_context = super::TokensCommandsContext)]
+#[interactive_clap(output_context = ViewFtBalanceContext)]
 pub struct ViewFtBalance {
-    ///What is the ft-contract account ID?
+    /// What is the ft-contract account ID?
     ft_contract_account_id: crate::types::account_id::AccountId,
     #[interactive_clap(named_arg)]
-    ///Select network
+    /// Select network
     network_config: crate::network_view_at_block::NetworkViewAtBlockArgs,
 }
 
-impl ViewFtBalance {
-    pub async fn process(
-        &self,
-        config: crate::config::Config,
-        owner_account_id: near_primitives::types::AccountId,
-    ) -> crate::CliResult {
-        let super::FtMetadata { decimals, symbol } = super::params_ft_metadata(
-            config.clone(),
-            self.ft_contract_account_id.clone(),
-            self.network_config.clone(),
-        )
-        .await?;
+#[derive(Clone)]
+pub struct ViewFtBalanceContext(crate::network_view_at_block::ArgsForViewContext);
 
-        let method_name = "ft_balance_of".to_string();
-        let args = json!({
-            "account_id": owner_account_id.to_string(),
-        })
-        .to_string()
-        .into_bytes();
-        let query_view_method_response = self
-            .network_config
-            .get_network_config(config)
-            .json_rpc_client()
-            .call(near_jsonrpc_client::methods::query::RpcQueryRequest {
-                block_reference: self.network_config.get_block_ref(),
-                request: near_primitives::views::QueryRequest::CallFunction {
-                    account_id: self.ft_contract_account_id.clone().into(),
-                    method_name,
-                    args: near_primitives::types::FunctionArgs::from(args),
-                },
-            })
-            .await
-            .map_err(|err| {
-                color_eyre::Report::msg(format!("Failed to fetch query for view method: {:?}", err))
-            })?;
-        let call_result =
-            if let near_jsonrpc_primitives::types::query::QueryResponseKind::CallResult(result) =
-                query_view_method_response.kind
-            {
-                result.result
-            } else {
-                return Err(color_eyre::Report::msg("Error call result".to_string()));
-            };
+impl ViewFtBalanceContext {
+    pub fn from_previous_context(
+        previous_context: super::TokensCommandsContext,
+        scope: &<ViewFtBalance as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> color_eyre::eyre::Result<Self> {
+        let owner_account_id = previous_context.owner_account_id;
+        let ft_contract_account_id: near_primitives::types::AccountId =
+            scope.ft_contract_account_id.clone().into();
 
-        let serde_call_result = if call_result.is_empty() {
-            serde_json::Value::Null
-        } else {
-            serde_json::from_slice(&call_result)
-                .map_err(|err| color_eyre::Report::msg(format!("serde json: {:?}", err)))?
-        };
-        let amount: String = serde_json::from_value(serde_call_result).unwrap();
-        let amount = amount.parse::<u128>().unwrap();
-        let amount_fmt = {
-            if amount == 0 {
-                format!("0 {}", symbol)
-            } else if (amount % 10u128.pow(decimals as u32)) == 0 {
-                format!("{} {}", amount / 10u128.pow(decimals as u32), symbol,)
-            } else {
-                format!(
-                    "{}.{} {}",
-                    amount / 10u128.pow(decimals as u32),
-                    format!("{:0>24}", amount % 10u128.pow(decimals as u32)).trim_end_matches('0'),
-                    symbol
-                )
+        let on_after_getting_block_reference_callback: crate::network_view_at_block::OnAfterGettingBlockReferenceCallback = std::sync::Arc::new({
+            move |network_config, block_reference| {
+                let super::FtMetadata { decimals, symbol } = tokio::runtime::Runtime::new()
+                    .unwrap()
+                    .block_on(super::params_ft_metadata(
+                        ft_contract_account_id.clone(),
+                        &network_config,
+                        block_reference.clone(),
+                        )
+                    )?;
+
+                let method_name = "ft_balance_of".to_string();
+                let args = json!({
+                    "account_id": owner_account_id.to_string(),
+                    })
+                    .to_string()
+                    .into_bytes();
+                let query_view_method_response = tokio::runtime::Runtime::new()
+                .unwrap()
+                .block_on(
+                    network_config
+                    .json_rpc_client()
+                    .call(near_jsonrpc_client::methods::query::RpcQueryRequest {
+                        block_reference: block_reference.clone(),
+                        request: near_primitives::views::QueryRequest::CallFunction {
+                            account_id: ft_contract_account_id.clone(),
+                            method_name,
+                            args: near_primitives::types::FunctionArgs::from(args),
+                        },
+                    }))
+                    .map_err(|err| {
+                        color_eyre::Report::msg(format!("Failed to fetch query for view method: {:?}", err))
+                    })?;
+                let call_result =
+                    if let near_jsonrpc_primitives::types::query::QueryResponseKind::CallResult(result) =
+                        query_view_method_response.kind
+                    {
+                        result.result
+                    } else {
+                        return Err(color_eyre::Report::msg("Error call result".to_string()));
+                    };
+                let serde_call_result = if call_result.is_empty() {
+                    serde_json::Value::Null
+                } else {
+                    serde_json::from_slice(&call_result)
+                        .map_err(|err| color_eyre::Report::msg(format!("serde json: {:?}", err)))?
+                };
+                let amount: String = serde_json::from_value(serde_call_result).unwrap();
+                let amount = amount.parse::<u128>().unwrap();
+                let amount_fmt = {
+                    if amount == 0 {
+                        format!("0 {}", symbol)
+                    } else if (amount % 10u128.pow(decimals as u32)) == 0 {
+                        format!("{} {}", amount / 10u128.pow(decimals as u32), symbol,)
+                    } else {
+                        format!(
+                            "{}.{} {}",
+                            amount / 10u128.pow(decimals as u32),
+                            format!("{:0>24}", amount % 10u128.pow(decimals as u32)).trim_end_matches('0'),
+                            symbol
+                        )
+                    }
+                };
+
+                println!(
+                    "\n<{}> account has {}  (FT-contract: {})",
+                    owner_account_id, amount_fmt, ft_contract_account_id
+                );
+                Ok(())
             }
-        };
+        });
+        Ok(Self(crate::network_view_at_block::ArgsForViewContext {
+            config: previous_context.config,
+            on_after_getting_block_reference_callback,
+        }))
+    }
+}
 
-        println!(
-            "\n<{}> account has {}  (FT-contract: {})",
-            owner_account_id, amount_fmt, self.ft_contract_account_id
-        );
-        Ok(())
+impl From<ViewFtBalanceContext> for crate::network_view_at_block::ArgsForViewContext {
+    fn from(item: ViewFtBalanceContext) -> Self {
+        item.0
     }
 }
