f88b9347d7cebeff0b965c52bb758bd398fcd10c
vova <frol_off@meta.ua>
2023-03-31 13:05:29 +0300

    self-update: wrap_err

--- src/commands/extensions/self_update/mod.rs
+++ src/commands/extensions/self_update/mod.rs
@@ -3,6 +3,8 @@ const BIN_NAME: &str = "near-cli.exe";
 #[cfg(not(windows))]
 const BIN_NAME: &str = "near-cli";
 
+use color_eyre::eyre::WrapErr;
+
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(input_context = crate::GlobalContext)]
 #[interactive_clap(output_context = SelfUpdateCommandContext)]
@@ -32,13 +34,9 @@ impl SelfUpdateCommandContext {
             .show_download_progress(true)
             .current_version(self_update::cargo_crate_version!())
             .build()
-            .map_err(|err| {
-                color_eyre::Report::msg(format!("Failed to build self_update: {:?}", err))
-            })?
+            .wrap_err("Failed to build self_update")?
             .update()
-            .map_err(|err| {
-                color_eyre::Report::msg(format!("Failed to update near-cli-rs: {:?}", err))
-            })?;
+            .wrap_err("Failed to update near-cli-rs")?;
         Ok(Self)
     }
 }
@@ -50,8 +48,8 @@ pub fn get_latest_version() -> color_eyre::eyre::Result<String> {
         .bin_name("near-cli")
         .current_version(self_update::cargo_crate_version!())
         .build()
-        .map_err(|err| color_eyre::Report::msg(format!("Failed to build self_update: {:?}", err)))?
+        .wrap_err("Failed to build self_update")?
         .get_latest_release()
-        .map_err(|err| color_eyre::Report::msg(format!("Failed to get latest release: {:?}", err)))?
+        .wrap_err("Failed to get latest release")?
         .version)
 }
