6d00fb1981dd6b1dac2ee8063e28ed024fc62889
Vlad Frolov <frolvlad@gmail.com>
2023-04-01 09:26:54 +0200

    refactor: refactored main.rs and fixed self-update recommendation command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::enum_variant_names, clippy::large_enum_variant)]
 use clap::Parser;
-use common::{try_external_subcommand_execution, CliResult};
+#[cfg(feature = "self-update")]
+use color_eyre::eyre::WrapErr;
 use interactive_clap::ToCliArgs;
 
 mod commands;
@@ -14,16 +15,17 @@ mod transaction_signature_options;
 mod types;
 mod utils_command;
 
+pub use common::CliResult;
 pub type GlobalContext = (crate::config::Config,);
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(context = crate::GlobalContext)]
 struct Cmd {
     #[interactive_clap(subcommand)]
-    top_level: self::commands::TopLevelCommand,
+    top_level: crate::commands::TopLevelCommand,
 }
 
-fn main() -> CliResult {
+fn main() -> crate::common::CliResult {
     let config = crate::common::get_config_toml()?;
 
     color_eyre::install()?;
@@ -33,24 +35,27 @@ fn main() -> CliResult {
         crate::commands::extensions::self_update::get_latest_version()
     });
 
+    let near_cli_exec_path = std::env::args()
+        .next()
+        .unwrap_or_else(|| "./near".to_owned());
+
     let cli = match Cmd::try_parse() {
         Ok(cli) => cli,
         Err(error) => {
             match error.kind() {
                 clap::error::ErrorKind::DisplayHelp | clap::error::ErrorKind::DisplayVersion => {}
-                _ => match self::js_command_match::JsCmd::try_parse() {
+                _ => match crate::js_command_match::JsCmd::try_parse() {
                     Ok(js_cmd) => {
                         match js_cmd.rust_command_generation() {
                             Ok(vec_cmd) => {
-                                let near_cli_exec_path = std::env::args()
-                                    .next()
-                                    .unwrap_or_else(|| "./near_cli".to_owned());
-                                let mut suggested_cmd = Vec::with_capacity(vec_cmd.len() + 1);
-                                suggested_cmd.push(near_cli_exec_path);
-                                suggested_cmd.extend(vec_cmd);
                                 println!("The command you tried to run is deprecated in the new NEAR CLI, but we tried our best to match the old command with the new syntax, try it instead:");
                                 println!();
-                                println!("{}", shell_words::join(suggested_cmd));
+                                println!(
+                                    "{}",
+                                    shell_words::join(
+                                        std::iter::once(near_cli_exec_path).chain(vec_cmd)
+                                    )
+                                );
                             }
                             Err(err) => {
                                 println!("The command you tried to run is deprecated in the new NEAR CLI and there is no equivalent command in the new NEAR CLI.");
@@ -68,93 +73,95 @@ fn main() -> CliResult {
                 },
             }
 
-            if matches!(
-                error.kind(),
-                clap::error::ErrorKind::UnknownArgument | clap::error::ErrorKind::InvalidSubcommand
-            ) {
-                return try_external_subcommand_execution(error);
+            if let clap::error::ErrorKind::UnknownArgument
+            | clap::error::ErrorKind::InvalidSubcommand = error.kind()
+            {
+                return crate::common::try_external_subcommand_execution(error);
             }
             error.exit();
         }
     };
 
-    #[allow(unused_variables)]
-    let cli_cmd = loop {
-        match <Cmd as interactive_clap::FromCli>::from_cli(Some(cli.clone()), (config.clone(),)) {
-            interactive_clap::ResultFromCli::Ok(cli_cmd)
-            | interactive_clap::ResultFromCli::Cancel(Some(cli_cmd)) => {
+    let cli_cmd = match <Cmd as interactive_clap::FromCli>::from_cli(Some(cli), (config,)) {
+        interactive_clap::ResultFromCli::Ok(cli_cmd)
+        | interactive_clap::ResultFromCli::Cancel(Some(cli_cmd)) => {
+            println!(
+                "Your console command:\n{}",
+                shell_words::join(
+                    std::iter::once(&near_cli_exec_path).chain(&cli_cmd.to_cli_args())
+                )
+            );
+            Ok(Some(cli_cmd))
+        }
+        interactive_clap::ResultFromCli::Cancel(None) => {
+            println!("Goodbye!");
+            Ok(None)
+        }
+        interactive_clap::ResultFromCli::Back => {
+            unreachable!("TopLevelCommand does not have back option");
+        }
+        interactive_clap::ResultFromCli::Err(optional_cli_cmd, err) => {
+            if let Some(cli_cmd) = optional_cli_cmd {
                 println!(
-                    "Your console command:\n{} {}",
-                    std::env::args().next().as_deref().unwrap_or("./near_cli"),
-                    shell_words::join(&cli_cmd.to_cli_args())
+                    "Your console command:\n{}",
+                    shell_words::join(
+                        std::iter::once(&near_cli_exec_path).chain(&cli_cmd.to_cli_args())
+                    )
                 );
-                break cli_cmd;
             }
-            interactive_clap::ResultFromCli::Cancel(None) => {
-                println!("Goodbye!");
-                return Ok(());
-            }
-            interactive_clap::ResultFromCli::Back => {}
-            interactive_clap::ResultFromCli::Err(optional_cli_cmd, err) => {
-                if let Some(cli_cmd) = optional_cli_cmd {
-                    println!(
-                        "Your console command:\n{} {}",
-                        std::env::args().next().as_deref().unwrap_or("./near_cli"),
-                        shell_words::join(&cli_cmd.to_cli_args())
-                    );
-                }
-                return Err(err);
-            }
+            Err(err)
         }
     };
 
     #[cfg(feature = "self-update")]
-    {
-        if !matches!(
-            cli_cmd,
-            CliCmd {
-                top_level: Some(crate::commands::CliTopLevelCommand::Extensions(
-                    crate::commands::extensions::CliExtensionsCommands {
-                        extensions_actions: Some(
-                            crate::commands::extensions::CliExtensionsActions::SelfUpdate(
-                                crate::commands::extensions::self_update::CliSelfUpdateCommand {},
-                            )
-                        ),
-                    },
-                )),
-            }
-        ) {
-            if let Ok(Ok(result)) = handle.join() {
-                let current_version = semver::Version::parse(self_update::cargo_crate_version!())
-                    .map_err(|err| {
-                    color_eyre::Report::msg(format!(
-                        "Failed to parse current version of near-cli-rs as Version: {:?}",
-                        err
-                    ))
-                })?;
+    // We don't need to check the version if user has just called self-update
+    if !matches!(
+        cli_cmd,
+        Ok(Some(CliCmd {
+            top_level: Some(crate::commands::CliTopLevelCommand::Extensions(
+                crate::commands::extensions::CliExtensionsCommands {
+                    extensions_actions: Some(
+                        crate::commands::extensions::CliExtensionsActions::SelfUpdate(
+                            crate::commands::extensions::self_update::CliSelfUpdateCommand {},
+                        )
+                    ),
+                },
+            )),
+        }))
+    ) {
+        if let Ok(Ok(latest_version)) = handle.join() {
+            let current_version = semver::Version::parse(self_update::cargo_crate_version!())
+                .wrap_err("Failed to parse current version of `near` CLI")?;
 
-                let latest_version = semver::Version::parse(result.as_str()).map_err(|err| {
-                    color_eyre::Report::msg(format!(
-                        "Failed to parse latest version of near-cli-rs as Version: {:?}",
-                        err
-                    ))
-                })?;
+            let latest_version = semver::Version::parse(&latest_version)
+                .wrap_err("Failed to parse latest version of `near` CLI")?;
 
-                if current_version < latest_version {
-                    println!(
-                        "\nNEAR-CLI-RS has a new update available \x1b[2m{}\x1b[0m →  \x1b[32m{}\x1b[0m",
-                        current_version.to_string(),
-                        latest_version.to_string()
-                    );
-
-                    println!(
-                        "To update NEAR-CLI-RS use: {} {}",
-                        std::env::args().next().as_deref().unwrap_or("./near_cli"),
-                        shell_words::join(cli_cmd.to_cli_args())
-                    );
-                }
+            if current_version < latest_version {
+                println!();
+                println!(
+                    "`near` CLI has a new update available \x1b[2m{current_version}\x1b[0m →  \x1b[32m{latest_version}\x1b[0m"
+                );
+                let self_update_cli_cmd = CliCmd {
+                    top_level:
+                        Some(crate::commands::CliTopLevelCommand::Extensions(
+                            crate::commands::extensions::CliExtensionsCommands {
+                                extensions_actions:
+                                    Some(crate::commands::extensions::CliExtensionsActions::SelfUpdate(
+                                        crate::commands::extensions::self_update::CliSelfUpdateCommand {},
+                                    )),
+                            },
+                        )),
+                };
+                println!(
+                    "To update `near` CLI use: {}",
+                    shell_words::join(
+                        std::iter::once(near_cli_exec_path)
+                            .chain(self_update_cli_cmd.to_cli_args())
+                    )
+                );
             }
         }
     };
-    Ok(())
+
+    cli_cmd.map(|_| ())
 }
