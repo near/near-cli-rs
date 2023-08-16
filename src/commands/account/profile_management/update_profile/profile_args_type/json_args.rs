113b085136b5b7d6a3b306b0af0fec1dcd92de1d
vova <frol_off@meta.ua>
2023-08-16 11:47:57 +0300

    refactored input_data()

--- src/commands/account/profile_management/update_profile/profile_args_type/json_args.rs
+++ src/commands/account/profile_management/update_profile/profile_args_type/json_args.rs
@@ -1,13 +1,11 @@
 use std::str::FromStr;
 
 use color_eyre::eyre::WrapErr;
-use inquire::Text;
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(input_context = super::super::UpdateAccountProfileContext)]
 #[interactive_clap(output_context = JsonArgsContext)]
 pub struct JsonArgs {
-    #[interactive_clap(skip_default_input_arg)]
     /// Input valid JSON arguments (e.g. {\"token_id\": \"42\"})"
     data: String,
     #[interactive_clap(named_arg)]
@@ -27,7 +25,9 @@ impl JsonArgsContext {
             global_context: previous_context.global_context,
             get_contract_account_id: previous_context.get_contract_account_id,
             account_id: previous_context.account_id,
-            data: scope.data.clone(),
+            data: serde_json::Value::from_str(&scope.data)
+                .wrap_err("Data not in JSON format!")?
+                .to_string(),
         }))
     }
 }
@@ -35,19 +35,5 @@ impl JsonArgsContext {
 impl From<JsonArgsContext> for super::ArgsContext {
     fn from(item: JsonArgsContext) -> Self {
         item.0
-    }
-}
-
-impl JsonArgs {
-    fn input_data(
-        _context: &super::super::UpdateAccountProfileContext,
-    ) -> color_eyre::eyre::Result<Option<String>> {
-        let data =
-            Text::new("Input valid JSON arguments (e.g. {\"token_id\": \"42\"})").prompt()?;
-        Ok(Some(
-            serde_json::Value::from_str(&data)
-                .wrap_err("Data not in JSON format!")?
-                .to_string(),
-        ))
     }
 }
