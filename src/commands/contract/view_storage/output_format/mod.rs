bd84067a860a1d9e1aee9522c20a5ef7d5ae04b6
FroVolod <frol_off@meta.ua>
2023-09-06 12:32:33 +0300

    fmt

--- src/commands/contract/view_storage/output_format/mod.rs
+++ src/commands/contract/view_storage/output_format/mod.rs
@@ -8,10 +8,14 @@ mod as_table;
 #[strum_discriminants(derive(EnumMessage, EnumIter))]
 /// Choose a format to view contract storage state:
 pub enum OutputFormat {
-    #[strum_discriminants(strum(message = "as-json      - View contract storage state in JSON format"))]
+    #[strum_discriminants(strum(
+        message = "as-json      - View contract storage state in JSON format"
+    ))]
     /// View contract storage state in JSON format
     AsJson(self::as_json::AsJson),
-    #[strum_discriminants(strum(message = "as-table     - View contract storage state in the table"))]
+    #[strum_discriminants(strum(
+        message = "as-table     - View contract storage state in the table"
+    ))]
     /// View contract storage state in the table
     AsTable(self::as_table::AsTable),
 }
