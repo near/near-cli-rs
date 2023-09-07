ff00d9c7dc251582fd702a9a150e51b0b5f268dd
FroVolod <frol_off@meta.ua>
2023-09-07 20:28:57 +0300

    added KeysStartWithBytesAsBase64 to view

---
+++ src/commands/contract/view_storage/keys_to_view/keys_start_with_bytes_as_base64.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(input_context = super::super::ViewStorageContext)]
+#[interactive_clap(output_context = KeysStartWithBytesAsBase64Context)]
+pub struct KeysStartWithBytesAsBase64 {
+    /// Enter the string that the keys begin with Base64 bytes (for example, "Uw=="):
+    keys_begin_with: String,
+    #[interactive_clap(subcommand)]
+    output_format: super::super::output_format::OutputFormat,
+}
+
+#[derive(Debug, Clone)]
+pub struct KeysStartWithBytesAsBase64Context(super::KeysContext);
+
+impl KeysStartWithBytesAsBase64Context {
+    pub fn from_previous_context(
+        previous_context: super::super::ViewStorageContext,
+        scope: &<KeysStartWithBytesAsBase64 as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> color_eyre::eyre::Result<Self> {
+        Ok(Self(super::KeysContext {
+            global_context: previous_context.global_context,
+            contract_account_id: previous_context.contract_account_id,
+            prefix: near_primitives::types::StoreKey::from(
+                near_primitives::serialize::from_base64(&scope.keys_begin_with)?,
+            ),
+        }))
+    }
+}
+
+impl From<KeysStartWithBytesAsBase64Context> for super::KeysContext {
+    fn from(item: KeysStartWithBytesAsBase64Context) -> Self {
+        item.0
+    }
+}
--- src/commands/contract/view_storage/keys_to_view/mod.rs
+++ src/commands/contract/view_storage/keys_to_view/mod.rs
@@ -1,6 +1,7 @@
 use strum::{EnumDiscriminants, EnumIter, EnumMessage};
 
 mod all_keys;
+mod keys_start_with_bytes_as_base64;
 mod keys_start_with_string;
 
 #[derive(Debug, EnumDiscriminants, Clone, interactive_clap::InteractiveClap)]
@@ -22,7 +23,7 @@ pub enum KeysToView {
         message = "keys-start-with-bytes-as-base64  - View contract storage state for keys that start with Base64 bytes (for example, \"Uw==\")"
     ))]
     /// View contract storage state for keys that start with Base64 bytes (for example, "Uw==")
-    KeysStartWithBytesAsBase64,
+    KeysStartWithBytesAsBase64(self::keys_start_with_bytes_as_base64::KeysStartWithBytesAsBase64),
 }
 
 #[derive(Debug, Clone)]
