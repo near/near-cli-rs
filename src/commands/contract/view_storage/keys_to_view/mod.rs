1ff0063b910a4ac12f4dd908b2d38074304fe2da
FroVolod <frol_off@meta.ua>
2023-09-07 11:47:00 +0300

    added KeysStartWithString to view contract storage

--- src/commands/contract/view_storage/keys_to_view/mod.rs
+++ src/commands/contract/view_storage/keys_to_view/mod.rs
@@ -1,6 +1,7 @@
 use strum::{EnumDiscriminants, EnumIter, EnumMessage};
 
 mod all_keys;
+mod keys_start_with_string;
 
 #[derive(Debug, EnumDiscriminants, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(context = super::ViewStorageContext)]
@@ -16,7 +17,7 @@ pub enum KeysToView {
         message = "keys-start-with-string           - View contract storage state for keys that start with a string (for example, \"S\")"
     ))]
     /// View contract storage state for keys that start with a string (for example, "S")
-    KeysStartWithString,
+    KeysStartWithString(self::keys_start_with_string::KeysStartWithString),
     #[strum_discriminants(strum(
         message = "keys-start-with-bytes-as-base64  - View contract storage state for keys that start with Base64 bytes (for example, \"Uw==\")"
     ))]
