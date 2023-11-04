b6435cc37cf4da4a4e86e8f46458882dbff94006
FroVolod <frol_off@meta.ua>
2023-11-04 14:01:24 +0200

    updated

--- src/js_command_match/proposals.rs
+++ src/js_command_match/proposals.rs
@@ -4,3 +4,14 @@ pub struct ProposalsArgs {
     #[clap(allow_hyphen_values = true, num_args = 0..)]
     _unknown_args: Vec<String>,
 }
+
+impl ProposalsArgs {
+    pub fn to_cli_args(&self, network_config: String) -> Vec<String> {
+        vec![
+            "validator".to_owned(),
+            "proposals".to_owned(),
+            "network-config".to_owned(),
+            network_config,
+        ]
+    }
+}
--- src/js_command_match/validators.rs
+++ src/js_command_match/validators.rs
@@ -5,3 +5,20 @@ pub struct ValidatorsArgs {
     #[clap(allow_hyphen_values = true, num_args = 0..)]
     _unknown_args: Vec<String>,
 }
+
+impl ValidatorsArgs {
+    pub fn to_cli_args(&self, network_config: String) -> Vec<String> {
+        let mut output_vec = vec![
+            "validator".to_owned(),
+            "validators".to_owned(),
+            "network-config".to_owned(),
+            network_config,
+        ];
+        if "current" == &self.epoch {
+            output_vec.push("now".to_owned())
+        } else if "next" == &self.epoch {
+            output_vec.push("next".to_owned())
+        };
+        output_vec
+    }
+}
