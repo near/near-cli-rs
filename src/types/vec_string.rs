97c4c9deb90f5ab14f00b7b90fab032afd05f238
FroVolod <frol_off@meta.ua>
2024-05-01 12:15:46 +0300

    start

--- src/types/vec_string.rs
+++ src/types/vec_string.rs
@@ -11,6 +11,9 @@ impl std::str::FromStr for VecString {
     type Err = color_eyre::eyre::ErrReport;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            return Ok(Self(vec![]));
+        }
         let vec_str: Vec<String> = s.split(',').map(|str| str.trim().to_string()).collect();
         Ok(Self(vec_str))
     }
