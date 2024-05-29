96a65615002062e3bb82718385d8e5f4f6dbab70
FroVolod <frol_off@meta.ua>
2024-05-29 20:51:18 +0300

    refactored

--- src/transaction_signature_options/send/mod.rs
+++ src/transaction_signature_options/send/mod.rs
@@ -1,6 +1,8 @@
 use color_eyre::owo_colors::OwoColorize;
 use tracing_indicatif::span_ext::IndicatifSpanExt;
 
+use crate::common::JsonRpcClientExt;
+
 #[derive(Debug, Clone, interactive_clap_derive::InteractiveClap)]
 #[interactive_clap(input_context = super::SubmitContext)]
 #[interactive_clap(output_context = SendContext)]
@@ -25,38 +27,10 @@ impl SendContext {
             super::SignedTransactionOrSignedDelegateAction::SignedTransaction(
                 signed_transaction,
             ) => {
-                let retries_number = 5;
-                let mut retries = (1..=retries_number).rev();
-                let transaction_info = loop {
-                    let transaction_info_result =
-                        tokio::runtime::Runtime::new()
-                            .unwrap()
-                            .block_on(sending_transaction(
-                                format!("{}", previous_context.network_config.rpc_url),
-                                &previous_context.network_config.json_rpc_client(),
-                                signed_transaction.clone(),
-                            ));
-                    match transaction_info_result {
-                        Ok(response) => {
-                            break response;
-                        }
-                        Err(ref err) => match crate::common::rpc_transaction_error(err) {
-                            Ok(message) => {
-                                if let Some(retries_left) = retries.next() {
-                                    sleep_after_error(
-                                        format!("{} (Previous attempt failed with error: `{}`. Will retry {} more times)",
-                                        previous_context.network_config.rpc_url,
-                                        message.red(),
-                                        retries_left)
-                                    );
-                                } else {
-                                    return Err(color_eyre::eyre::eyre!(err.to_string()));
-                                }
-                            }
-                            Err(report) => return Err(color_eyre::Report::msg(report)),
-                        },
-                    };
-                };
+                let transaction_info = sending_signed_transaction(
+                    &previous_context.network_config,
+                    &signed_transaction,
+                )?;
 
                 crate::common::print_transaction_status(
                     &transaction_info,
@@ -98,34 +72,51 @@ impl SendContext {
     }
 }
 
+#[tracing::instrument(name = "Broadcasting transaction via RPC", skip_all)]
+pub fn sending_signed_transaction(
+    network_config: &crate::config::NetworkConfig,
+    signed_transaction: &near_primitives::transaction::SignedTransaction,
+) -> color_eyre::Result<near_primitives::views::FinalExecutionOutcomeView> {
+    tracing::Span::current().pb_set_message(network_config.rpc_url.as_str());
+    let retries_number = 5;
+    let mut retries = (1..=retries_number).rev();
+    let transaction_info = loop {
+        let transaction_info_result = network_config.json_rpc_client().blocking_call(
+            near_jsonrpc_client::methods::broadcast_tx_commit::RpcBroadcastTxCommitRequest {
+                signed_transaction: signed_transaction.clone(),
+            },
+        );
+        match transaction_info_result {
+            Ok(response) => {
+                break response;
+            }
+            Err(ref err) => match crate::common::rpc_transaction_error(err) {
+                Ok(message) => {
+                    if let Some(retries_left) = retries.next() {
+                        sleep_after_error(
+                            format!("{} (Previous attempt failed with error: `{}`. Will retry {} more times)",
+                            network_config.rpc_url,
+                            message.red(),
+                            retries_left)
+                        );
+                    } else {
+                        return Err(color_eyre::eyre::eyre!(err.to_string()));
+                    }
+                }
+                Err(report) => return Err(color_eyre::Report::msg(report)),
+            },
+        };
+    };
+    Ok(transaction_info)
+}
+
 #[tracing::instrument(
     name = "Waiting 5 seconds before broadcasting transaction via RPC",
     skip_all
 )]
-fn sleep_after_error(additional_message_for_name: String) {
+pub fn sleep_after_error(additional_message_for_name: String) {
     tracing::Span::current().pb_set_message(&additional_message_for_name);
     std::thread::sleep(std::time::Duration::from_secs(5));
-}
-
-#[tracing::instrument(name = "Broadcasting transaction via RPC", skip_all)]
-async fn sending_transaction(
-    additional_message_for_name: String,
-    json_rpc_client: &near_jsonrpc_client::JsonRpcClient,
-    signed_transaction: near_primitives::transaction::SignedTransaction,
-) -> Result<
-    near_primitives::views::FinalExecutionOutcomeView,
-    near_jsonrpc_client::errors::JsonRpcError<
-        near_jsonrpc_primitives::types::transactions::RpcTransactionError,
-    >,
-> {
-    tracing::Span::current().pb_set_message(&additional_message_for_name);
-    json_rpc_client
-        .call(
-            near_jsonrpc_client::methods::broadcast_tx_commit::RpcBroadcastTxCommitRequest {
-                signed_transaction: signed_transaction.clone(),
-            },
-        )
-        .await
 }
 
 #[tracing::instrument(name = "Broadcasting delegate action via a relayer url", skip_all)]
