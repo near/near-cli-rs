38345ea281c7cce361be2e660e546a2ae687cf61
garikbesson <garikbesson@gmail.com>
2024-06-29 19:19:02 +0200

    AddKeyArgs::to_cli_args unit tests

--- src/js_command_match/add_key.rs
+++ src/js_command_match/add_key.rs
@@ -1,15 +1,21 @@
+use crate::js_command_match::parameter_aliases::{
+  CONTRACT_ID_ALIASES,
+  METHOD_NAMES_ALIASES,
+  NETWORK_ID_ALIASES
+};
+
 #[derive(Debug, Clone, clap::Parser)]
 /// This is a legacy `add-key` command. Once you run it with the specified arguments, new syntax command will be suggested.
 pub struct AddKeyArgs {
     account_id: String,
     access_key: String,
-    #[clap(long, aliases = ["contractId"], default_value = None)]
+    #[clap(long, aliases = CONTRACT_ID_ALIASES, default_value = None)]
     contract_id: Option<String>,
-    #[clap(long, aliases = ["methodNames"], requires = "contract_id", value_delimiter = ',', num_args = 1..)]
+    #[clap(long, aliases = METHOD_NAMES_ALIASES, requires = "contract_id", value_delimiter = ',', num_args = 1..)]
     method_names: Vec<String>,
     #[clap(long, default_value = "0")]
     allowance: String,
-    #[clap(long, aliases = ["networkId"], default_value=None)]
+    #[clap(long, aliases = NETWORK_ID_ALIASES, default_value=None)]
     network_id: Option<String>,
     #[clap(allow_hyphen_values = true, num_args = 0..)]
     _unknown_args: Vec<String>,
@@ -17,32 +23,32 @@ pub struct AddKeyArgs {
 
 impl AddKeyArgs {
     pub fn to_cli_args(&self, network_config: String) -> Vec<String> {
-        let network_id = self.network_id.clone().unwrap_or(network_config.to_owned());
+        let network_id = self.network_id.clone().unwrap_or(network_config);
 
         let mut command = vec![
-            "account".to_owned(),
-            "add-key".to_owned(),
+            "account".to_string(),
+            "add-key".to_string(),
             self.account_id.to_owned(),
         ];
 
         if let Some(contract_id) = self.contract_id.as_deref() {
-            command.push("grant-function-call-access".to_owned());
-            command.push("--allowance".to_owned());
+            command.push("grant-function-call-access".to_string());
+            command.push("--allowance".to_string());
             command.push(format!("{} NEAR", self.allowance));
-            command.push("--receiver-account-id".to_owned());
+            command.push("--receiver-account-id".to_string());
             command.push(contract_id.to_owned());
-            command.push("--method-names".to_owned());
+            command.push("--method-names".to_string());
             command.push(self.method_names.join(","));
         } else {
-            command.push("grant-full-access".to_owned());
+            command.push("grant-full-access".to_string());
         }
           
-        command.push("use-manually-provided-public-key".to_owned());
+        command.push("use-manually-provided-public-key".to_string());
         command.push(self.access_key.to_owned());
-        command.push("network-config".to_owned());
+        command.push("network-config".to_string());
         command.push(network_id);
-        command.push("sign-with-keychain".to_owned());
-        command.push("send".to_owned());
+        command.push("sign-with-keychain".to_string());
+        command.push("send".to_string());
 
         command
     }
@@ -50,69 +56,102 @@ impl AddKeyArgs {
 
 #[cfg(test)]
 mod tests {
+    use clap::Parser;
+
     use super::*;
 
     #[test]
-    fn add_function_call_access_key_testnet() {
-        let account_id = "bob.testnet".to_string();
-        let access_key = "access_key_placeholder".to_string();
-        let contract_id = "example.testnet".to_string();
+    fn add_function_call_access_key_for_all_methods_testnet() {
+        let account_id = "bob.testnet";
+        let access_key = "ed25519:DReZmNmnGhpsYcCFFeYgPsJ9YCm9xH16GGujCPe3KQEq";
+        let contract_id = "example.testnet";
 
-        let add_key_args = AddKeyArgs {
-            account_id: account_id.clone(),
-            access_key: access_key.clone(),
-            contract_id: Some(contract_id.clone()),
-            method_names: [].to_vec(),
-            allowance: "0".to_string(),
-            network_id: None,
-            _unknown_args: [].to_vec(),
-        };
-        let result = AddKeyArgs::to_cli_args(&add_key_args, "testnet".to_string());
-        assert_eq!(
-            result.join(" "),
-            format!("account add-key {} grant-function-call-access --allowance 0 NEAR --receiver-account-id {} --method-names  use-manually-provided-public-key {} network-config testnet sign-with-keychain send", account_id, contract_id, access_key)
-        )
+        for i in 0..CONTRACT_ID_ALIASES.len() {
+            let contract_id_parameter_alias = &format!("--{}", &CONTRACT_ID_ALIASES[i]);
+            let add_key_args = AddKeyArgs::parse_from(&[
+                "near",
+                account_id,
+                access_key,
+                contract_id_parameter_alias,
+                contract_id,
+            ]);
+            let result = AddKeyArgs::to_cli_args(&add_key_args, "testnet".to_string());
+            assert_eq!(
+                result.join(" "),
+                format!("account add-key {} grant-function-call-access --allowance 0 NEAR --receiver-account-id {} --method-names  use-manually-provided-public-key {} network-config testnet sign-with-keychain send", account_id, contract_id, access_key)
+            )
+        }
+    }
+
+    #[test]
+    fn add_function_call_access_key_for_some_methods_testnet() {
+        let account_id = "bob.testnet";
+        let access_key = "ed25519:DReZmNmnGhpsYcCFFeYgPsJ9YCm9xH16GGujCPe3KQEq";
+        let contract_id = "example.testnet";
+        let method_names = "get,set";
+
+        for i in 0..METHOD_NAMES_ALIASES.len() {
+            let method_names_parameter_alias = &format!("--{}", &METHOD_NAMES_ALIASES[i]);
+            let contract_id_parameter_alias = &format!("--{}", &CONTRACT_ID_ALIASES[0]);
+            let add_key_args = AddKeyArgs::parse_from(&[
+                "near",
+                account_id,
+                access_key,
+                contract_id_parameter_alias,
+                contract_id,
+                method_names_parameter_alias,
+                method_names
+            ]);
+            let result = AddKeyArgs::to_cli_args(&add_key_args, "testnet".to_string());
+            assert_eq!(
+                result.join(" "),
+                format!("account add-key {} grant-function-call-access --allowance 0 NEAR --receiver-account-id {} --method-names {} use-manually-provided-public-key {} network-config testnet sign-with-keychain send", account_id, contract_id, method_names, access_key)
+            )
+        }
     }
 
     #[test]
     fn add_full_access_key_testnet() {
-        let account_id = "bob.testnet".to_string();
-        let access_key = "access_key_placeholder".to_string();
+        let account_id = "bob.testnet";
+        let access_key = "ed25519:DReZmNmnGhpsYcCFFeYgPsJ9YCm9xH16GGujCPe3KQEq";
 
-        let add_key_args = AddKeyArgs {
-            account_id: account_id.clone(),
-            access_key: access_key.clone(),
-            contract_id: None,
-            method_names: [].to_vec(),
-            allowance: "0".to_string(),
-            network_id: None,
-            _unknown_args: [].to_vec(),
-        };
-        let result = AddKeyArgs::to_cli_args(&add_key_args, "testnet".to_string());
-        assert_eq!(
-            result.join(" "),
-            format!("account add-key {} grant-full-access use-manually-provided-public-key {} network-config testnet sign-with-keychain send", account_id, access_key)
-        )
+        for i in 0..NETWORK_ID_ALIASES.len() {
+            let network_id_parameter_alias = &format!("--{}", &NETWORK_ID_ALIASES[i]);
+            let add_key_args = AddKeyArgs::parse_from(&[
+                "near",
+                account_id,
+                access_key,
+                network_id_parameter_alias,
+                "testnet"
+            ]);
+            let result = AddKeyArgs::to_cli_args(&add_key_args, "testnet".to_string());
+            assert_eq!(
+                result.join(" "),
+                format!("account add-key {} grant-full-access use-manually-provided-public-key {} network-config testnet sign-with-keychain send", account_id, access_key)
+            )
+        }
     }
 
     #[test]
     fn add_full_access_key_mainnet() {
-        let account_id = "bob.testnet".to_string();
-        let access_key = "access_key_placeholder".to_string();
+        let account_id = "bob.testnet";
+        let access_key = "ed25519:DReZmNmnGhpsYcCFFeYgPsJ9YCm9xH16GGujCPe3KQEq";
+        let network_id = "mainnet";
 
-        let add_key_args = AddKeyArgs {
-            account_id: account_id.clone(),
-            access_key: access_key.clone(),
-            contract_id: None,
-            method_names: [].to_vec(),
-            allowance: "0".to_string(),
-            network_id: None,
-            _unknown_args: [].to_vec(),
-        };
-        let result = AddKeyArgs::to_cli_args(&add_key_args, "mainnet".to_string());
-        assert_eq!(
-            result.join(" "),
-            format!("account add-key {} grant-full-access use-manually-provided-public-key {} network-config mainnet sign-with-keychain send", account_id, access_key)
-        )
+        for i in 0..NETWORK_ID_ALIASES.len() {
+            let network_id_parameter_alias = &format!("--{}", &NETWORK_ID_ALIASES[i]);
+            let add_key_args = AddKeyArgs::parse_from(&[
+                "near",
+                account_id,
+                access_key,
+                network_id_parameter_alias,
+                network_id
+            ]);
+            let result = AddKeyArgs::to_cli_args(&add_key_args, "testnet".to_string());
+            assert_eq!(
+                result.join(" "),
+                format!("account add-key {} grant-full-access use-manually-provided-public-key {} network-config {} sign-with-keychain send", account_id, access_key, network_id)
+            )
+        }
     }
 }
--- src/js_command_match/parameter_aliases.rs
+++ src/js_command_match/parameter_aliases.rs
@@ -1,9 +1,20 @@
-pub const USE_FAUCET_ALIASES: [&str; 2] = ["useFaucet", "use-faucet"];
+// Accounts and contracts
 pub const MASTER_ACCOUNT_ALIASES: [&str; 6] = ["masterAccount", "master-account", "useAccount", "use-account", "accountId", "account_id"];
-pub const SEED_PHRASE_ALIASES: [&str; 2] = ["seedPhrase", "seed-phrase"];
+pub const CONTRACT_ID_ALIASES: [&str; 2] = ["contractId", "contract-id"];
+pub const METHOD_NAMES_ALIASES: [&str; 2] = ["methodNames", "method-names"];
+
+// Keys
 pub const PUBLIC_KEY_ALIASES: [&str; 2] = ["publicKey", "public-key"];
 pub const SECRET_KEY_ALIASES: [&str; 2] = ["secretKey", "secret-key"];
+pub const SEED_PHRASE_ALIASES: [&str; 2] = ["seedPhrase", "seed-phrase"];
+
+// Ledger
 pub const USE_LEDGER_ALIASES: [&str; 4] = ["signWithLedger", "sign-with-ledger", "useLedgerKey", "use-ledger-key"];
 pub const LEDGER_PATH_ALIASES: [&str; 2] = ["ledgerPath", "ledger-path"];
+
+// Balance and faucet
 pub const INITIAL_BALANCE_ALIASES: [&str; 2] = ["initialBalance", "initial-balance"];
+pub const USE_FAUCET_ALIASES: [&str; 2] = ["useFaucet", "use-faucet"];
+
+// Network config
 pub const NETWORK_ID_ALIASES: [&str; 2] = ["networkId", "network-id"];
