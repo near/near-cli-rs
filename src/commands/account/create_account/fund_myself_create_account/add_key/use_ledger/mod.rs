14fc6d6adc6352318f7dbf2a24d500993b535eaa
FroVolod <autorti@gmail.com>
2024-07-06 18:44:17 +0300

    feat: Added the ability to select HD Path from the ledger (#362)

--- src/commands/account/create_account/fund_myself_create_account/add_key/use_ledger/mod.rs
+++ src/commands/account/create_account/fund_myself_create_account/add_key/use_ledger/mod.rs
@@ -2,6 +2,9 @@
 #[interactive_clap(input_context = super::super::NewAccountContext)]
 #[interactive_clap(output_context = AddAccessWithLedgerContext)]
 pub struct AddAccessWithLedger {
+    #[interactive_clap(long)]
+    #[interactive_clap(skip_default_input_arg)]
+    seed_phrase_hd_path: crate::types::slip10::BIP32Path,
     #[interactive_clap(named_arg)]
     /// What is the signer account ID?
     sign_as: super::super::sign_as::SignerAccountId,
@@ -13,9 +16,9 @@ pub struct AddAccessWithLedgerContext(super::super::AccountPropertiesContext);
 impl AddAccessWithLedgerContext {
     pub fn from_previous_context(
         previous_context: super::super::NewAccountContext,
-        _scope: &<AddAccessWithLedger as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+        scope: &<AddAccessWithLedger as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
     ) -> color_eyre::eyre::Result<Self> {
-        let seed_phrase_hd_path = crate::transaction_signature_options::sign_with_ledger::SignLedger::input_seed_phrase_hd_path()?.unwrap();
+        let seed_phrase_hd_path = scope.seed_phrase_hd_path.clone();
         eprintln!(
             "Please allow getting the PublicKey on Ledger device (HD Path: {})",
             seed_phrase_hd_path
@@ -51,5 +54,13 @@ impl AddAccessWithLedgerContext {
 impl From<AddAccessWithLedgerContext> for super::super::AccountPropertiesContext {
     fn from(item: AddAccessWithLedgerContext) -> Self {
         item.0
+    }
+}
+
+impl AddAccessWithLedger {
+    pub fn input_seed_phrase_hd_path(
+        _context: &super::super::NewAccountContext,
+    ) -> color_eyre::eyre::Result<Option<crate::types::slip10::BIP32Path>> {
+        crate::transaction_signature_options::sign_with_ledger::input_seed_phrase_hd_path()
     }
 }
