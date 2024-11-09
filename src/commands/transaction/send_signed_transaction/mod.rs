721baadde468f96cb4ab91fdb1f5cf3ed60a522e
FroVolod <frol_off@meta.ua>
2024-11-09 20:53:11 +0200

    start

--- src/commands/transaction/send_signed_transaction/mod.rs
+++ src/commands/transaction/send_signed_transaction/mod.rs
@@ -1,14 +1,31 @@
+use color_eyre::eyre::WrapErr;
+use strum::{EnumDiscriminants, EnumIter, EnumMessage};
+
 mod network;
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
-#[interactive_clap(input_context = crate::GlobalContext)]
-#[interactive_clap(output_context = SignedTransactionContext)]
+#[interactive_clap(context = crate::GlobalContext)]
 pub struct SignedTransaction {
-    /// Enter a signed transaction as base64-encoded string:
-    signed_action: crate::types::signed_transaction::SignedTransactionAsBase64,
-    #[interactive_clap(named_arg)]
-    /// Select network
-    network_config: self::network::Network,
+    #[interactive_clap(subcommand)]
+    /// Select the base64 signed transaction input method
+    signed_transaction_type: SignedTransactionType,
+}
+
+#[derive(Debug, EnumDiscriminants, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(context = crate::GlobalContext)]
+#[strum_discriminants(derive(EnumMessage, EnumIter))]
+/// Select the Base64 signed transaction input method:
+pub enum SignedTransactionType {
+    #[strum_discriminants(strum(
+        message = "base64-signed-transaction  - Base64-encoded string (e.g. e30=)"
+    ))]
+    /// Base64-encoded string (e.g. e30=)
+    Base64SignedTransaction(Base64SignedTransaction),
+    #[strum_discriminants(strum(
+        message = "file                       - Read from file (e.g. reusable JSON or binary data)"
+    ))]
+    /// Read from file (e.g. reusable JSON or binary data)
+    File(File),
 }
 
 #[derive(Debug, Clone)]
@@ -17,14 +34,79 @@ pub struct SignedTransactionContext {
     signed_transaction: near_primitives::transaction::SignedTransaction,
 }
 
-impl SignedTransactionContext {
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(input_context = crate::GlobalContext)]
+#[interactive_clap(output_context = Base64SignedTransactionContext)]
+pub struct Base64SignedTransaction {
+    /// Enter a signed transaction as base64-encoded string:
+    signed_action: crate::types::signed_transaction::SignedTransactionAsBase64,
+    #[interactive_clap(named_arg)]
+    /// Select network
+    network_config: self::network::Network,
+}
+
+#[derive(Debug, Clone)]
+pub struct Base64SignedTransactionContext(SignedTransactionContext);
+
+impl Base64SignedTransactionContext {
     pub fn from_previous_context(
         previous_context: crate::GlobalContext,
-        scope: &<SignedTransaction as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+        scope: &<Base64SignedTransaction as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
     ) -> color_eyre::eyre::Result<Self> {
-        Ok(Self {
+        Ok(Self(SignedTransactionContext {
             config: previous_context.config,
             signed_transaction: scope.signed_action.inner.clone(),
-        })
+        }))
+    }
+}
+
+impl From<Base64SignedTransactionContext> for SignedTransactionContext {
+    fn from(item: Base64SignedTransactionContext) -> Self {
+        item.0
+    }
+}
+
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(input_context = crate::GlobalContext)]
+#[interactive_clap(output_context = FileContext)]
+pub struct File {
+    /// Enter the path to the file with the transaction as a string in base64 encoding:
+    file_path: crate::types::path_buf::PathBuf,
+    #[interactive_clap(named_arg)]
+    /// Select network
+    network_config: self::network::Network,
+}
+
+#[derive(Debug, Clone)]
+pub struct FileContext(SignedTransactionContext);
+
+#[derive(Debug, serde::Deserialize)]
+struct FileSignedTransaction {
+    #[serde(alias = "Signed transaction (serialized as base64)")]
+    signed_transaction: near_primitives::transaction::SignedTransaction,
+}
+
+impl FileContext {
+    pub fn from_previous_context(
+        previous_context: crate::GlobalContext,
+        scope: &<File as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> color_eyre::eyre::Result<Self> {
+        let data = std::fs::read_to_string(&scope.file_path)
+            .wrap_err_with(|| format!("File {:?} not found!", &scope.file_path))?;
+
+        let signed_transaction = serde_json::from_str::<FileSignedTransaction>(&data)
+            .wrap_err_with(|| format!("Error reading data from file: {:?}", &scope.file_path))?
+            .signed_transaction;
+
+        Ok(Self(SignedTransactionContext {
+            config: previous_context.config,
+            signed_transaction,
+        }))
+    }
+}
+
+impl From<FileContext> for SignedTransactionContext {
+    fn from(item: FileContext) -> Self {
+        item.0
     }
 }
