83c1f8ec9b82cb66f7cc0710ecd8bc27a0755bdd
FroVolod <frol_off@meta.ua>
2025-05-15 21:15:56 +0300

    added flags: use_contract_source_code_path, save_contract_source_code_into

--- src/commands/contract/verify/mod.rs
+++ src/commands/contract/verify/mod.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use color_eyre::{
     eyre::{Context, ContextCompat},
     owo_colors::OwoColorize,
@@ -13,14 +15,48 @@ use near_verify_rs::types::{
 use crate::common::JsonRpcClientExt;
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
-#[interactive_clap(context = crate::GlobalContext)]
+#[interactive_clap(input_context = crate::GlobalContext)]
+#[interactive_clap(output_context = ContractContext)]
 pub struct Contract {
+    #[interactive_clap(long)]
+    #[interactive_clap(skip_interactive_input)]
+    use_contract_source_code_path: Option<crate::types::utf8_path_buf::Utf8PathBuf>,
+    #[interactive_clap(long)]
+    #[interactive_clap(skip_interactive_input)]
+    save_contract_source_code_into: Option<crate::types::utf8_path_buf::Utf8PathBuf>,
     #[interactive_clap(subcommand)]
     source_contract_code: SourceContractCode,
 }
 
+#[derive(Debug, Clone)]
+pub struct ContractContext {
+    global_context: crate::GlobalContext,
+    use_contract_source_code_path: Option<crate::types::utf8_path_buf::Utf8PathBuf>,
+    save_contract_source_code_into: Option<crate::types::utf8_path_buf::Utf8PathBuf>,
+}
+
+impl ContractContext {
+    pub fn from_previous_context(
+        previous_context: crate::GlobalContext,
+        scope: &<Contract as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> color_eyre::eyre::Result<Self> {
+        if scope.use_contract_source_code_path.is_some()
+            & scope.save_contract_source_code_into.is_some()
+        {
+            return color_eyre::eyre::Result::Err(color_eyre::eyre::eyre!(
+                "You are using the `--use-contract-source-code-path` and `--save-contract-source-code-into` options at the same time. This is not allowed."
+            ));
+        }
+        Ok(Self {
+            global_context: previous_context,
+            use_contract_source_code_path: scope.use_contract_source_code_path.clone(),
+            save_contract_source_code_into: scope.save_contract_source_code_into.clone(),
+        })
+    }
+}
+
 #[derive(Debug, EnumDiscriminants, Clone, interactive_clap::InteractiveClap)]
-#[interactive_clap(context = crate::GlobalContext)]
+#[interactive_clap(context = ContractContext)]
 #[strum_discriminants(derive(EnumMessage, EnumIter))]
 /// Choose the code source for the contract:
 pub enum SourceContractCode {
@@ -37,7 +73,7 @@ pub enum SourceContractCode {
 }
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
-#[interactive_clap(input_context = crate::GlobalContext)]
+#[interactive_clap(input_context = ContractContext)]
 #[interactive_clap(output_context = ContractAccountIdContext)]
 pub struct ContractAccountId {
     #[interactive_clap(skip_default_input_arg)]
@@ -50,10 +86,10 @@ pub struct ContractAccountId {
 
 impl ContractAccountId {
     pub fn input_contract_account_id(
-        context: &crate::GlobalContext,
+        context: &ContractContext,
     ) -> color_eyre::eyre::Result<Option<crate::types::account_id::AccountId>> {
         crate::common::input_non_signer_account_id_from_used_account_list(
-            &context.config.credentials_home_dir,
+            &context.global_context.config.credentials_home_dir,
             "What is the contract account ID?",
         )
     }
@@ -64,22 +100,37 @@ pub struct ContractAccountIdContext(crate::network_view_at_block::ArgsForViewCon
 
 impl ContractAccountIdContext {
     pub fn from_previous_context(
-        previous_context: crate::GlobalContext,
+        previous_context: ContractContext,
         scope: &<ContractAccountId as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
     ) -> color_eyre::eyre::Result<Self> {
         let on_after_getting_block_reference_callback: crate::network_view_at_block::OnAfterGettingBlockReferenceCallback = std::sync::Arc::new({
             let account_id: near_primitives::types::AccountId = scope.contract_account_id.clone().into();
 
             move |network_config, block_reference| {
+                let contract_source_code = if let Some(path) = &previous_context.use_contract_source_code_path {
+                    path.read_bytes()?
+                } else {
+                    let docker_build_code = get_contract_code_from_repository(&account_id, network_config, block_reference)?;
+                    if let Some(path) = &previous_context.save_contract_source_code_into {
+                        std::fs::File::create(path)
+                            .wrap_err_with(|| format!("Failed to create file: {:?}", &path))?
+                            .write(&docker_build_code)
+                            .wrap_err_with(|| format!("Failed to write to file: {:?}", &path))?;
+                        tracing::info!("The file '{}' was downloaded successfully", path);
+                    }
+                    docker_build_code
+                };
+
                 verify_contract(
-                    get_contract_code_hash_from_repository(&account_id, network_config, block_reference)?,
-                    get_contract_code_hash_from_contract_account_id(&account_id, network_config, block_reference)?
+                    contract_source_code,
+                    get_contract_code_from_contract_account_id(&account_id, network_config, block_reference)?
                 );
+
                 Ok(())
             }
         });
         Ok(Self(crate::network_view_at_block::ArgsForViewContext {
-            config: previous_context.config,
+            config: previous_context.global_context.config,
             on_after_getting_block_reference_callback,
             interacting_with_account_ids: vec![scope.contract_account_id.clone().into()],
         }))
@@ -93,7 +144,7 @@ impl From<ContractAccountIdContext> for crate::network_view_at_block::ArgsForVie
 }
 
 #[derive(Debug, Clone, interactive_clap_derive::InteractiveClap)]
-#[interactive_clap(input_context = crate::GlobalContext)]
+#[interactive_clap(input_context = ContractContext)]
 #[interactive_clap(output_context = ContractFileContext)]
 pub struct ContractFile {
     /// What is a file location of the contract?
@@ -104,57 +155,59 @@ pub struct ContractFileContext;
 
 impl ContractFileContext {
     pub fn from_previous_context(
-        _previous_context: crate::GlobalContext,
+        previous_context: ContractContext,
         scope: &<ContractFile as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
     ) -> color_eyre::eyre::Result<Self> {
-        let wasm = std::fs::read(&scope.file_path).wrap_err_with(|| {
-            format!("Failed to open or read the file: {:?}.", &scope.file_path,)
-        })?;
+        let wasm_code = scope.file_path.read_bytes()?;
 
-        let contract_source_metadata = tokio::runtime::Runtime::new()
-            .unwrap()
-            .block_on(fetch_contract_source_metadata_from_wasm(&wasm))?;
-
-        let (_tempdir, docker_build_out_wasm) =
-            get_docker_build_out_wasm_from_contract_source_metadata(contract_source_metadata)?;
-
-        let docker_build_code = std::fs::read(&docker_build_out_wasm).wrap_err_with(|| {
-            format!(
-                "Failed to open or read the file: {:?}.",
-                &docker_build_out_wasm,
-            )
-        })?;
-
-        if wasm == docker_build_code {
-            tracing::info!("\n{}", crate::common::indent_payload(
-                "The code obtained from the WASM file and the code calculated from the repository are the same."
-            ))
+        let contract_source_code = if let Some(path) =
+            previous_context.use_contract_source_code_path
+        {
+            path.read_bytes()?
         } else {
-            tracing::info!("\n{}", crate::common::indent_payload(
-                "The code obtained from the WASM file and the code calculated from the repository are different."
-            ).red())
-        }
+            let contract_source_metadata = tokio::runtime::Runtime::new()
+                .unwrap()
+                .block_on(fetch_contract_source_metadata_from_wasm(&wasm_code))?;
+
+            let (_tempdir, docker_build_out_wasm) =
+                get_docker_build_out_wasm_from_contract_source_metadata(contract_source_metadata)?;
+
+            let docker_build_code = std::fs::read(&docker_build_out_wasm).wrap_err_with(|| {
+                format!(
+                    "Failed to open or read the file: {:?}.",
+                    &docker_build_out_wasm,
+                )
+            })?;
+            if let Some(path) = previous_context.save_contract_source_code_into {
+                std::fs::File::create(&path)
+                    .wrap_err_with(|| format!("Failed to create file: {:?}", &path))?
+                    .write(&docker_build_code)
+                    .wrap_err_with(|| format!("Failed to write to file: {:?}", &path))?;
+                tracing::info!("The file '{}' was downloaded successfully", path);
+            }
+            docker_build_code
+        };
+
+        verify_contract(contract_source_code, wasm_code);
 
         Ok(Self)
     }
 }
 
-fn verify_contract(
-    contract_code_hash_from_repository: near_verify_rs::types::sha256_checksum::SHA256Checksum,
-    contract_code_hash: near_primitives::hash::CryptoHash,
-) {
-    if contract_code_hash_from_repository.hash == contract_code_hash.as_bytes() {
+fn verify_contract(contract_code_from_repository: Vec<u8>, contract_code: Vec<u8>) {
+    // XXX fix message
+    if contract_code_from_repository == contract_code {
         tracing::info!("\n{}", crate::common::indent_payload(&
             format!(
-            "The hash code obtained from the contract account ID and the hash code calculated from the repository are the same.\nhash: {}",
-            contract_code_hash
+            "The hash code obtained from the contract account ID and the hash code calculated from the repository are the same.\nhash: ",
+            // contract_code_hash
         )))
     } else {
         tracing::info!("\n{}", crate::common::indent_payload(&
             format!(
-            "The hash code obtained from the contract account ID: {}\nThe hash code calculated from the repository:        {}",
-            contract_code_hash,
-            contract_code_hash_from_repository.to_base58_string()
+            "The hash code obtained from the contract account ID: \nThe hash code calculated from the repository:        ",
+            // contract_code_hash,
+            // contract_code_hash_from_repository.to_base58_string()
         )).red())
     };
 }
@@ -163,11 +216,11 @@ fn verify_contract(
     name = "Getting the contract code hash from the repository ...",
     skip_all
 )]
-fn get_contract_code_hash_from_repository(
+fn get_contract_code_from_repository(
     account_id: &near_primitives::types::AccountId,
     network_config: &crate::config::NetworkConfig,
     block_reference: &near_primitives::types::BlockReference,
-) -> color_eyre::eyre::Result<near_verify_rs::types::sha256_checksum::SHA256Checksum> {
+) -> color_eyre::eyre::Result<Vec<u8>> {
     let contract_source_metadata = tokio::runtime::Runtime::new().unwrap().block_on(
         super::inspect::get_contract_source_metadata(
             &network_config.json_rpc_client(),
@@ -178,7 +231,12 @@ fn get_contract_code_hash_from_repository(
     let (_tempdir, docker_build_out_wasm) =
         get_docker_build_out_wasm_from_contract_source_metadata(contract_source_metadata)?;
 
-    near_verify_rs::logic::compute_hash(docker_build_out_wasm)
+    std::fs::read(&docker_build_out_wasm).wrap_err_with(|| {
+        format!(
+            "Failed to open or read the file: {:?}.",
+            &docker_build_out_wasm,
+        )
+    })
 }
 
 #[tracing::instrument(
@@ -234,11 +292,11 @@ fn checkout_remote_repo(
     name = "Getting the contract code hash from the contract account ID ...",
     skip_all
 )]
-fn get_contract_code_hash_from_contract_account_id(
+fn get_contract_code_from_contract_account_id(
     account_id: &near_primitives::types::AccountId,
     network_config: &crate::config::NetworkConfig,
     block_reference: &near_primitives::types::BlockReference,
-) -> color_eyre::eyre::Result<near_primitives::hash::CryptoHash> {
+) -> color_eyre::eyre::Result<Vec<u8>> {
     let view_code_response = network_config
         .json_rpc_client()
         .blocking_call(near_jsonrpc_client::methods::query::RpcQueryRequest {
@@ -262,7 +320,7 @@ fn get_contract_code_hash_from_contract_account_id(
         } else {
             return Err(color_eyre::Report::msg("Error call result".to_string()));
         };
-    Ok(contract_code_view.hash)
+    Ok(contract_code_view.code)
 }
 
 async fn fetch_contract_source_metadata_from_wasm(
