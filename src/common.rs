1a645fc5820dd8d6d3180d28cf4ed5c657e793e0
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2025-06-12 14:23:54 -0400

    fix: nested display of json args

--- src/common.rs
+++ src/common.rs
@@ -4,6 +4,7 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::str::FromStr;
 
+use base64::Engine;
 use color_eyre::eyre::{ContextCompat, WrapErr};
 use color_eyre::owo_colors::OwoColorize;
 use futures::{StreamExt, TryStreamExt};
@@ -714,6 +715,35 @@ pub fn print_full_unsigned_transaction(
     info_str
 }
 
+pub fn deep_decode_args(v: &mut serde_json::Value) {
+    match v {
+        serde_json::Value::Object(map) => {
+            for (key, val) in map.iter_mut() {
+                if key == "args" {
+                    if let serde_json::Value::String(s) = val {
+                        if let Ok(bytes) = base64::engine::general_purpose::STANDARD.decode(s) {
+                            if let Ok(mut nested) =
+                                serde_json::from_slice::<serde_json::Value>(&bytes)
+                            {
+                                deep_decode_args(&mut nested);
+                                *val = nested;
+                                continue;
+                            }
+                        }
+                    }
+                }
+                deep_decode_args(val);
+            }
+        }
+        serde_json::Value::Array(arr) => {
+            for elem in arr {
+                deep_decode_args(elem);
+            }
+        }
+        _ => {}
+    }
+}
+
 pub fn print_unsigned_transaction(
     transaction: &crate::commands::PrepopulatedTransaction,
 ) -> String {
@@ -764,14 +794,15 @@ pub fn print_unsigned_transaction(
                     "",
                     "args:",
                     match serde_json::from_slice::<serde_json::Value>(&function_call_action.args) {
-                        Ok(parsed_args) => {
-                            serde_json::to_string_pretty(&parsed_args)
+                        Ok(mut parsed) => {
+                            deep_decode_args(&mut parsed);
+                            serde_json::to_string_pretty(&parsed)
                                 .unwrap_or_else(|_| "".to_string())
                                 .replace('\n', "\n                                 ")
                         }
                         Err(_) => {
-                            if let Ok(args) = String::from_utf8(function_call_action.args.clone()) {
-                                args
+                            if let Ok(s) = String::from_utf8(function_call_action.args.clone()) {
+                                s
                             } else {
                                 format!(
                                     "<non-printable data ({})>",
