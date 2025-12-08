e97f59b7d8c80dc3af44400be11e42809e6665b3
Vlad Frolov <frolvlad@gmail.com>
2025-12-08 09:31:20 +0100

    aligned the styles with cliclack

--- src/lib.rs
+++ src/lib.rs
@@ -91,34 +91,26 @@ pub fn setup_tracing(verbosity: Verbosity) -> CliResult {
 pub fn get_global_render_config() -> RenderConfig<'static> {
     let mut render_config = RenderConfig::default_colored();
     render_config.prompt_prefix = Styled::new("◆ ").with_fg(Color::DarkGreen);
-    render_config.answered_prompt_prefix = Styled::new("◇ ").with_fg(Color::DarkGrey);
+    render_config.answered_prompt_prefix = Styled::new("◇ ").with_fg(Color::DarkGreen);
     render_config.highlighted_option_prefix = Styled::new(" ●").with_fg(Color::DarkGreen);
     render_config.unhighlighted_option_prefix = Styled::new(" ○").with_fg(Color::DarkGrey);
     render_config.selected_checkbox = Styled::new("◼").with_fg(Color::LightGreen);
-    render_config.scroll_up_prefix = Styled::new("⇞");
-    render_config.scroll_down_prefix = Styled::new("⇟");
+    render_config.scroll_up_prefix = Styled::new("↑○").with_fg(Color::DarkGrey);
+    render_config.scroll_down_prefix = Styled::new("↓○").with_fg(Color::DarkGrey);
     render_config.unselected_checkbox = Styled::new("◻").with_fg(Color::DarkGrey);
+    render_config.option = StyleSheet::new().with_fg(Color::DarkGrey);
+    render_config.selected_option = Some(StyleSheet::new().with_fg(Color::Grey));
 
-    render_config.selected_option = Some(
-        StyleSheet::new()
-            .with_fg(Color::DarkGreen)
-            .with_attr(Attributes::ITALIC),
-    );
-
-    render_config.new_line_prefix = Some(Styled::new("│  ").with_fg(Color::DarkGrey));
+    render_config.new_line_prefix = Some(Styled::new("│  ").with_fg(Color::LightBlue));
     render_config.answer_from_new_line = true;
 
     render_config.error_message = render_config
         .error_message
         .with_prefix(Styled::new("❌").with_fg(Color::LightRed));
 
-    render_config.text_input = StyleSheet::new()
-        .with_fg(Color::LightYellow)
-        .with_attr(Attributes::ITALIC);
+    render_config.text_input = StyleSheet::new().with_fg(Color::LightYellow);
 
-    render_config.answer = StyleSheet::new()
-        .with_attr(Attributes::ITALIC)
-        .with_fg(Color::DarkGrey);
+    render_config.answer = StyleSheet::new().with_fg(Color::DarkGrey);
 
     render_config.help_message = StyleSheet::new().with_fg(Color::DarkYellow);
 
