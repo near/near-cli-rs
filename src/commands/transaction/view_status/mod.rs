a43a3929d12734d0eb112fef349d58a38c34e725
FroVolod <frol_off@meta.ua>
2025-12-31 10:32:40 +0200

    refactored "transaction" commands

--- src/commands/transaction/view_status/mod.rs
+++ src/commands/transaction/view_status/mod.rs
@@ -29,14 +29,14 @@ impl TransactionInfoContext {
                 move |network_config| {
                     let query_view_transaction_status =
                         get_transaction_info(network_config, tx_hash)?;
-                    if let crate::Verbosity::Quiet = previous_context.verbosity {
-                        println!("Transaction status:\n{query_view_transaction_status:#?}");
+                    if let crate::Verbosity::Interactive | crate::Verbosity::TeachMe =
+                        &previous_context.verbosity
+                    {
+                        eprintln!("Transaction status:");
+                        println!("{query_view_transaction_status:#?}");
+                    } else {
+                        println!("{query_view_transaction_status:?}");
                     }
-                    tracing::info!(
-                        parent: &tracing::Span::none(),
-                        "Transaction status:\n{}",
-                        crate::common::indent_payload(&format!("{query_view_transaction_status:#?}"))
-                    );
                     Ok(())
                 }
             });
